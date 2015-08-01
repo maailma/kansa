34317bd19b5a31728cb13ec5c046f3b207c165f9
Kyle Unverferth <kyle@kyleu.com>
2015-08-01 10:33:41 -0600

    Resolving table creation race condition.

--- app/services/database/Schema.scala
+++ app/services/database/Schema.scala
@@ -20,16 +20,18 @@ object Schema {
   )
 
   def update() = {
-    Future.sequence(tables.map { t =>
-      Database.query(DdlQueries.DoesTableExist(t._1)).flatMap { exists =>
-        if (exists) {
-          Future.successful(Unit)
-        } else {
-          Logger.info(s"Creating missing table [${t._1}].")
-          val name = s"CreateTable-${t._1}"
-          Database.raw(name, t._2.sql).map(x => Unit)
+    tables.foldLeft(Future.successful(Unit)) { (f, t) =>
+      f.flatMap { u =>
+        Database.query(DdlQueries.DoesTableExist(t._1)).flatMap { exists =>
+          if (exists) {
+            Future.successful(Unit)
+          } else {
+            Logger.info(s"Creating missing table [${t._1}].")
+            val name = s"CreateTable-${t._1}"
+            Database.raw(name, t._2.sql).map(x => Unit)
+          }
         }
       }
-    })
+    }
   }
 }
