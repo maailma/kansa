913a14c5b6a1d221bd246a851e30169dd7ca2827
Petri Laihonen <pietu@worldcon.fi>
2016-01-01 22:12:36 +0200

    Initial Stripe tests

--- src/main/scala/servlets.scala
+++ src/main/scala/servlets.scala
@@ -7,7 +7,17 @@ class HelloServlet extends HttpServlet {
   override def doGet(request: HttpServletRequest, response: HttpServletResponse) {
     response.setContentType("text/html")
     response.setCharacterEncoding("UTF-8")
-    response.getWriter.write("""<h1>Hello, world!</h1>""")
+    response.getWriter.write("""<h1>Hello, worldcon 75!</h1>""")
+  }
+
+}
+
+class StripeServlet extends HttpServlet {
+
+  override def doGet(request: HttpServletRequest, response: HttpServletResponse) {
+    response.setContentType("text/html")
+    response.setCharacterEncoding("UTF-8")
+    response.getWriter.write("""<h1>There will be stripe stuff here!</h1>""")
   }
 
 }
