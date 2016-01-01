0aa359e5d589f2526659d68814b9e61b7a44df1c
Petri Laihonen <pietu@worldcon.fi>
2016-01-01 23:30:53 +0200

    Got some basic stuff working

--- src/main/scala/servlets.scala
+++ src/main/scala/servlets.scala
@@ -1,6 +1,7 @@
 package servlets
 
 import javax.servlet.http._
+// import java.util.Enumeration;
 
 class HelloServlet extends HttpServlet {
 
@@ -14,10 +15,24 @@ class HelloServlet extends HttpServlet {
 
 class StripeServlet extends HttpServlet {
 
+  val myOutput = "<h1>There will be stripe stuff here!</h1>";
+
   override def doGet(request: HttpServletRequest, response: HttpServletResponse) {
-    response.setContentType("text/html")
-    response.setCharacterEncoding("UTF-8")
-    response.getWriter.write("""<h1>There will be stripe stuff here!</h1>""")
+
+    response.setContentType("text/html");
+    response.setCharacterEncoding("UTF-8");
+    response.getWriter.write(myOutput);
+    // response.getWriter.write("""<h1>There will be stripe stuff here!</h1>""")
+
+    var params = request.getParameterNames(); 
+    while(params.hasMoreElements()){
+      val paramName = params.nextElement();
+      val myOutput = "Attribute Name: <strong>"+paramName+"</strong>, Value: <strong>"+request.getParameter(paramName)+"</strong><br>"
+      response.getWriter.write(myOutput);
+    }
+
+    response.getWriter.write("Request Method: <strong>" + request.getMethod() + "</strong>");
+
   }
 
 }
