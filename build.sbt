14cbd08072e1fa44d800ef72080a866e5a6d66fc
Petri Laihonen <pietu@worldcon.fi>
2016-01-03 19:53:23 +0200

    Now the servlet is accepting POST requests from another domain (CORS) for test purposes. I also added possible logging into a file, though I have no clue where the file /tmp/test.log is being written to. (What is the root for writing files.)

--- build.sbt
+++ build.sbt
@@ -1,6 +1,7 @@
 enablePlugins(TomcatPlugin)
 
 libraryDependencies += "javax.servlet" % "javax.servlet-api" % "3.0.1" % "provided"
+libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.1.3"
 
 lazy val root = (project in file(".")).
   settings(
--- src/main/scala/servlets.scala
+++ src/main/scala/servlets.scala
@@ -1,7 +1,9 @@
 package servlets
 
 import javax.servlet.http._
-// import java.util.Enumeration;
+import org.slf4j.LoggerFactory
+import ch.qos.logback.core.util.StatusPrinter
+import ch.qos.logback.classic.LoggerContext
 
 class HelloServlet extends HttpServlet {
 
@@ -15,24 +17,35 @@ class HelloServlet extends HttpServlet {
 
 class StripeServlet extends HttpServlet {
 
-  val myOutput = "<h1>There will be stripe stuff here!</h1>";
+  // def logger = LoggerFactory.getLogger(this.getClass) 
+  
+  /*
+  println("- About to log!")
+  logger.info("Pietu's log test STDOUT")
+  logger.debug("Pietu's log test FILE")
+  */
 
-  override def doGet(request: HttpServletRequest, response: HttpServletResponse) {
+  // override def doGet(request: HttpServletRequest, response: HttpServletResponse) {
+  override def doPost(request: HttpServletRequest, response: HttpServletResponse) {
 
-    response.setContentType("text/html");
-    response.setCharacterEncoding("UTF-8");
-    response.getWriter.write(myOutput);
-    // response.getWriter.write("""<h1>There will be stripe stuff here!</h1>""")
+    response.setContentType("text/html")
+    response.setCharacterEncoding("UTF-8")
+    response.setHeader("Access-Control-Allow-Origin", "*");       // Need to add the correct domain in here!!
+    //response.setHeader("Access-Control-Allow-Methods", "POST");   // Only allow POST, moot with "doPost"
+    response.setHeader("Access-Control-Max-Age", "300");          // Cache response for 5 minutes
+    response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); // Ensure this header is also allowed!  
 
-    var params = request.getParameterNames(); 
+    // response.getWriter.write("<h2>There will be stripe stuff here!</h2>")
+    // response.getWriter.write("Request Method: <strong>" + request.getMethod() + "</strong><br>")
+    println("Request Method: " + request.getMethod())
+
+    var params = request.getParameterNames() 
     while(params.hasMoreElements()){
-      val paramName = params.nextElement();
-      val myOutput = "Attribute Name: <strong>"+paramName+"</strong>, Value: <strong>"+request.getParameter(paramName)+"</strong><br>"
-      response.getWriter.write(myOutput);
+      val paramName = params.nextElement()
+      // val myOutput = "Attribute Name: <strong>"+paramName+"</strong>, Value: <strong>"+request.getParameter(paramName)+"</strong><br>"
+      // response.getWriter.write(myOutput)
+      println("POST VAR: " + paramName + " = " + request.getParameter(paramName))
     }
-
-    response.getWriter.write("Request Method: <strong>" + request.getMethod() + "</strong>");
-
   }
 
 }
