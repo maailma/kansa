428c59f295890a66a2d68f56153c341ea2caa7dc
Petri Laihonen <pietu@worldcon.fi>
2016-01-06 22:13:38 +0200

    Added some placeholder code for charge. (Java example from Stripe.com)

--- src/main/scala/servlets.scala
+++ src/main/scala/servlets.scala
@@ -76,4 +76,28 @@ class StripeServlet extends HttpServlet {
     return myString
   }
 
+  /*
+  def charge() = {
+    // Set your secret key: remember to change this to your live secret key in production
+    // See your keys here https://dashboard.stripe.com/account/apikeys
+    Stripe.apiKey = getSecretKey()
+
+    // Get the credit card details submitted by the form
+    String token = request.getParameter("stripeToken");
+
+    // Create the charge on Stripe's servers - this will charge the user's card
+    try {
+      Map<String, Object> chargeParams = new HashMap<String, Object>();
+      chargeParams.put("amount", 1000); // amount in cents, again
+      chargeParams.put("currency", "usd");
+      chargeParams.put("source", token);
+      chargeParams.put("description", "Example charge");
+
+      Charge charge = Charge.create(chargeParams);
+    } catch (CardException e) {
+      // The card has been declined
+    }
+
+  }
+  */
 }
