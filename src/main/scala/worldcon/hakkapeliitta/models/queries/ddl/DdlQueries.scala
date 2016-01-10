2ef8cfb1b024fa9a587c48c9b7e3c93aa6be0548
Ian Monroe <imonroe@kde.org>
2016-01-10 14:59:18 -0800

    rearrange code to prepare merge into worldcon repo

---
+++ src/main/scala/worldcon/hakkapeliitta/models/queries/ddl/DdlQueries.scala
@@ -0,0 +1,18 @@
+package worldcon.hakkapeliitta.models.queries.ddl
+
+import jdub.async.{ Row, SingleRowQuery }
+
+import scala.util.Try
+
+object DdlQueries {
+  case class DoesTableExist(tableName: String) extends SingleRowQuery[Boolean] {
+    override val sql = "select exists (select * from information_schema.tables WHERE table_name = ?);"
+    override val values = tableName :: Nil
+    override def map(row: Row) = {
+      Try(row.as[Boolean]("exists")).toOption match {
+        case Some(true) => true
+        case _ => false
+      }
+    }
+  }
+}
---
+++ src/main/scala/worldcon/hakkapeliitta/models/user/RegistrationData.scala
@@ -0,0 +1,7 @@
+package worldcon.hakkapeliitta.models.user
+
+case class RegistrationData(
+  username: String,
+  email: String,
+  password: String
+)
