a2b575eaa70c38c977b83eddd052aca75939d889
Ian Monroe <imonroe@kde.org>
2016-01-18 19:58:04 -0800

    remove Tomcat SSL stuff
    now we're using nginx

--- ssl/build-ssl-tomcat.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# By running this script, you can create Tomcat server container with self signed certificate.
-# Please note that the server.xml has been configured to use "changeit" password.
-#
-sudo openssl req -newkey 2048 -nodes -keyout worldcon.key -x509 -days 365 -out worldcon.crt
-sudo openssl pkcs12 -inkey worldcon.key -in worldcon.crt -export -out worldcon.pfx
-docker build -t tomcat_ssl .
