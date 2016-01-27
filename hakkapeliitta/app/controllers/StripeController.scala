6617b5ba7da21a53f0acacd62138009e1470946b
Eemeli Aro <eemeli@gmail.com>
2016-01-27 02:19:20 +0200

    Write transactions to disk as JSON

--- hakkapeliitta/app/controllers/StripeController.scala
+++ hakkapeliitta/app/controllers/StripeController.scala
@@ -1,24 +1,27 @@
 package controllers
 
+import java.nio.file.{Paths, Files}
+import java.nio.charset.StandardCharsets
+import java.time.LocalDateTime
 import java.util.HashMap
 
 import com.stripe.Stripe
 import com.stripe.exception._
 import com.stripe.model.Charge
+import com.stripe.model.StripeObject
 import play.api.Play
 import play.api.mvc.Action
 import play.api.mvc.Results._
 import play.api.Play.current
 
 import scala.concurrent.Future
+import scala.util.parsing.json.JSONObject
 
 class StripeController {
   val notJsonResult= BadRequest("Invalid, request not made in JSON")
 
   def orderMembership = Action { request =>
     request.body.asJson.map { json =>
-      println("And the secret is")
-      println(Play.application.configuration.getString("stripe.apiKey"))
       (for {
         apiKey <- Play.application.configuration.getString("stripe.apiKey")
         tokenId <- (json \ "tokenId").asOpt[String]
@@ -36,13 +39,24 @@ class StripeController {
         chargeParams.put("description", description)
 
         try {
+          println(s"Processing transaction $tokenId...")
           val result = Charge.create(chargeParams)
+          println(s"Transaction $tokenId status: ${result.getStatus}")
+          val jsonHeaders = JSONObject(request.headers.toSimpleMap)
+          val jsonResult = StripeObject.PRETTY_PRINT_GSON.toJson(result)
+          val jsonLog = s"""{\n"headers": $jsonHeaders,\n"request": $json,\n"response": $jsonResult\n}\n"""
+          val logFileName = s"$tokenId--${LocalDateTime.now}"
+          val path = Paths.get(s"/transactions/$logFileName")
+          Files.write(path, jsonLog.getBytes(StandardCharsets.UTF_8))
+          println(s"Transaction complete, stored as `$logFileName`.")
           Ok(s"yep $result")
         } catch {
           case e: StripeException =>
-            val errorStr = s"Error making Strip request.\nRequest id: ${e.getRequestId}\nStatus code${e.getStatusCode}"
+            val errorStr = s"Error making Stripe request.\nRequest id: ${e.getRequestId}\nStatus code${e.getStatusCode}"
             InternalServerError(errorStr)
-          case _ : Throwable => InternalServerError("Unknown issue making Stripe request.")
+          case e: Throwable =>
+            e.printStackTrace
+            InternalServerError(s"Unknown issue making Stripe request:\n$e")
         }
       }).getOrElse(BadRequest(s"Missing parameters in request\n$json\n${(json \ "descr").asOpt[String]}"))
     }.getOrElse(notJsonResult)
