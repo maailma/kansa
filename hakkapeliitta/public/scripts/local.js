d4864aa2c4d6f0392e9f0cee8f297723f96a9a0e
Eemeli Aro <eemeli@gmail.com>
2016-02-14 23:59:02 +0200

    Updated prices & layout

--- hakkapeliitta/public/scripts/local.js
+++ hakkapeliitta/public/scripts/local.js
@@ -1,7 +1,7 @@
 var memberships = {
-  adult: { currency: "eur", amount: 9500, description: "Adult Membership" },
-  youth: { currency: "eur", amount: 8000, description: "Youth Membership" },
-  child: { currency: "eur", amount: 5500, description: "Child Membership" },
+  adult: { currency: "eur", amount: 12000, description: "Adult Membership" },
+  youth: { currency: "eur", amount: 9000, description: "Youth Membership" },
+  child: { currency: "eur", amount: 6000, description: "Child Membership" },
   kidInTow: { currency: "eur", amount: 0, description: "Kid-in-tow Membership" },
   support: { currency: "eur", amount: 3500, description: "Supporting Membership" },
   firstCon: { currency: "eur", amount: 9500, description: "First Worldcon Membership" },
