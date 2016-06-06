d6ad90630d3d86eaa3fd3c59a7fb360ca0d11bb7
Eemeli Aro <eemeli@gmail.com>
2016-06-07 00:17:20 +0300

    Use env var DATABASE_URL rather than hard-coding in queries.js

--- db-api/queries.js
+++ db-api/queries.js
@@ -1,7 +1,7 @@
 const options = { promiseLib: require('bluebird') };
 const pgp = require('pg-promise')(options);
 require('pg-monitor').attach(options);
-const db = pgp('postgres://localhost:5432/worldcon75');
+const db = pgp(process.env.DATABASE_URL);
 
 module.exports = { getLog, getEveryone, getSinglePerson, addPerson, updatePuppy, removePuppy };
 
