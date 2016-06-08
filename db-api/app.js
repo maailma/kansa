ac348f69d8a035c756d492f387fe3de8c8da7322
Eemeli Aro <eemeli@gmail.com>
2016-06-08 23:13:23 +0300

    Remove cookie-parser as unused

--- db-api/app.js
+++ db-api/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const logger = require('morgan');
-const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 
@@ -12,7 +11,6 @@ const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(session({
   name: 'w75',
   secret: process.env.SESSION_SECRET,
