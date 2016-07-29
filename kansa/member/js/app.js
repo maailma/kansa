8ff4763f899c4c8d1c84ba559cf80aaf6bd750b7
Kimmo <kimmo@netzionale.com>
2016-07-29 09:55:38 +0300

    separete API call api from Login component 

--- kansa/member/js/app.js
+++ kansa/member/js/app.js
@@ -6,55 +6,7 @@ import 'whatwg-fetch';
 import Login from './components/Login.js';
 import App from './components/App.js';
 
-export default class API {
-  constructor(root) {
-    this.root = root;  // [scheme]://[host]:[port]/[path]/
-  }
-
-  static parse(response) {
-    if (response.ok) return response.json();
-    const error = new Error(response.statusText);
-    error.response = response;
-    throw error;
-  }
-
-  static queryFromObj(obj) {
-    return !obj ? '' : '?' + Object.keys(obj)
-      .map(k => k + '=' + encodeURIComponent(obj[k]))
-      .join('&');
-  }
-
-  path(cmd, params) {
-    return this.root + cmd + API.queryFromObj(params);
-  }
-
-  GET(cmd, params) {
-    const uri = this.path(cmd, params);
-    return fetch(uri, { credentials: 'include' })
-      .then(response => API.parse(response));
-  }
-
-  POST(cmd, body) {
-    const uri = this.path(cmd);
-    const opt = { credentials: 'include', method: 'POST' };
-    if (typeof body == 'string') {
-      opt.body = body;
-    } else {
-      opt.headers = { 'Content-Type': 'application/json' };
-      opt.body = JSON.stringify(body);
-    }
-    return fetch(uri, opt)
-      .then(response => API.parse(response));
-  }
-}
-
-const apiHost = '139.162.147.227:3000';
-const api = new API(`http://${apiHost}/`);
-
 /*** Would here catch query parameters or show form with email and key **/
-
-api.GET('login',{email:'admin@example.com',key:'key'})
-  .then(console.log('log'))
 
 
 let routes = (
--- kansa/member/js/components/Login.js
+++ kansa/member/js/components/Login.js
@@ -1,8 +1,16 @@
 import React from 'react';  
 import { Link } from 'react-router'
+import API from '../api.js'
 
-// ...
+const apiHost = '139.162.147.227:3000';
+const api = new API(`http://${apiHost}/`);
+
 export default React.createClass({
+	componentDidMount() {
+	api.GET('login',{email:this.props.params.email,key:this.props.params.key})
+  		.then(console.log('log'))
+	},	
+
   render() {
     return (
       <div>
