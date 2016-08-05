407409b5566028b8f3e3d66e808e4d2e8388d5c4
Eemeli Aro <eemeli@gmail.com>
2016-08-05 18:06:53 +0300

    App: ESnext-ify, use material-ui TextField & RaisedButton for form elements

--- kansa/member/js/components/App.jsx
+++ kansa/member/js/components/App.jsx
@@ -2,6 +2,8 @@ import { Map } from 'immutable';
 import React from 'react';  
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 import API from '../api.js';
 
 export default class App extends React.Component { 
@@ -11,33 +13,31 @@ export default class App extends React.Component {
     user: React.PropTypes.instanceOf(Map).isRequired
   }
 
-  constructor(props) {
-    super(props);
-    this.state = {};
+  state = {
+    email: '',
+    key: ''
   }
 
   componentWillReceiveProps(nextProps) {
-    if(nextProps.user.get("member")) {
+    if (nextProps.user.get('member')) {
       this.redirectToProfile();
     }
   }
 
-  handleEmailChange(e) {
-    this.setState({email: e.target.value});
-  }
+  handleEmailChange = ev => this.setState({ email: ev.target.value });
 
-  handleKeyChange(e) {
-    this.setState({key: e.target.value});
-  }
+  handleKeyChange = ev => this.setState({ key: ev.target.value });
 
-  handleLogin() {
-    this.props.api.GET('login',{email:this.state.email,key:this.state.key})
+  handleLogin = () => {
+    const { email, key } = this.state;
+    this.props.api.GET('login', { email, key })
       .then(location.reload());
   }
 
-  getKey() {
-    this.props.api.POST('key', {email : this.state.email})
-      .then(function(response) { })
+  getKey = () => {
+    const { email } = this.state;
+    this.props.api.POST('key', { email })
+      .then(res => { })
       .catch(e => console.log(e));
   }
 
@@ -46,7 +46,7 @@ export default class App extends React.Component {
   }
 
   validateLogin() {
-    if(this.state == null || this.state.email =="" || this.state.key =="") {
+    if(!this.state.email || !this.state.key) {
       msg = "Please fill out all fields";
       return false;
     }  
@@ -55,30 +55,45 @@ export default class App extends React.Component {
 
   render() {
     const { user } = this.props;
+    const { email, key } = this.state;
+    const validEmail = email && /.@.*\../.test(email);
 
     return <div>
       <hgroup>
         <h1>WORLDCON 75 LOGIN</h1>
       </hgroup>
-      <form>
-        <div className="group">
-          <input type="text" name="email" id="email" onChange={this.handleEmailChange.bind(this)}/>
-          <span className="highlight"></span><span className="bar"></span>
-          <label>Email</label>
-        </div>
-        <div className="group">
-          <input type="text" name="key" id="key" onChange={this.handleKeyChange.bind(this)} />
-          <span className="highlight"></span><span className="bar"></span>
-          <label>Key</label>
-        </div>
-        <button type="button" className="button buttonBlue" onClick={this.handleLogin.bind(this)}>
-          Login
-          <div className="ripples buttonRipples"><span className="ripplesCircle"></span></div>
-        </button>
-        <button type="button" className="button buttonBlue" onClick={this.getKey.bind(this)}>
-          Get login key
-          <div className="ripples buttonRipples"><span className="ripplesCircle"></span></div>
-        </button>
+      <form style={{ paddingTop: '1em' }}>
+        <TextField
+          id="email"
+          fullWidth={true}
+          floatingLabelText="Email"
+          value={email}
+          onChange={this.handleEmailChange}
+        />
+        <TextField
+          id="key"
+          fullWidth={true}
+          floatingLabelText="Key"
+          value={key}
+          onChange={this.handleKeyChange}
+        />
+        <div style={{ height: 32 }} />
+        <RaisedButton
+          label="Login"
+          fullWidth={true}
+          primary={true}
+          disabled={!validEmail || !key}
+          style={{ margin: '12px 0' }}
+          onTouchTap={this.handleLogin}
+        />
+        <RaisedButton
+          label="Send login key"
+          fullWidth={true}
+          primary={true}
+          disabled={!validEmail}
+          style={{ margin: '12px 0' }}
+          onTouchTap={this.getKey}
+        />
       </form>
     </div>
   }
