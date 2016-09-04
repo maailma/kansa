f68d0c40a13f6c34a217e55354b587b007292e5e
Eemeli Aro <eemeli@gmail.com>
2016-09-04 23:45:11 +0300

    Add Toolbar to App component, with top-right nav menu

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,72 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { push } from 'react-router-redux'
 
+import FlatButton from 'material-ui/FlatButton'
+import Menu from 'material-ui/Menu'
+import MenuItem from 'material-ui/MenuItem'
+import Popover from 'material-ui/Popover'
 import Snackbar from 'material-ui/Snackbar'
+import { Toolbar, ToolbarGroup, ToolbarTitle } from 'material-ui/Toolbar';
 
 import { hideMessage } from '../actions/app'
+import { logout } from '../actions/auth'
+import { TITLE } from '../constants'
 
-const App = ({ children, route: { title }, message, hideMessage }) => <div>
-  <h1>{title}</h1>
-  {children}
+class ButtonMenu extends React.Component {
+  static propTypes = {
+    label: React.PropTypes.string
+  }
+
+  state = {
+    anchorEl: null,
+    open: false
+  }
+
+  handleTouchTap = (event) => {
+    event.preventDefault();
+    this.setState({
+      anchorEl: event.currentTarget,
+      open: true
+    });
+  }
+
+  render = () => !this.props.label ? null : <div>
+    <FlatButton
+      onTouchTap={this.handleTouchTap}
+      label={this.props.label}
+            labelStyle={{ color: 'rgba(0, 0, 0, 0.4)', textTransform: 'none', verticalAlign: 'initial' }}
+            style={{ marginTop: 10 }}
+    />
+    <Popover
+      open={this.state.open}
+      anchorEl={this.state.anchorEl}
+      anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+      targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+      onRequestClose={ () => this.setState({ open: false }) }
+    >
+      <Menu>
+        { this.props.children }
+      </Menu>
+    </Popover>
+  </div>;
+}
+
+const App = ({ children, email, goHome, hideMessage, logout, message }) => <div>
+   <Toolbar>
+    <ToolbarGroup>
+      <ToolbarTitle text={TITLE} />
+    </ToolbarGroup>
+    <ToolbarGroup>
+      <ButtonMenu label={email}>
+        <MenuItem primaryText="Home" onTouchTap={goHome} />
+        <MenuItem primaryText="Sign out" onTouchTap={logout} />
+      </ButtonMenu>
+    </ToolbarGroup>
+  </Toolbar>
+  <main>
+    {children}
+  </main>
   <Snackbar
     open={!!message}
     message={message}
@@ -17,9 +76,12 @@ const App = ({ children, route: { title }, message, hideMessage }) => <div>
 
 export default connect(
   (state) => ({
+    email: state.user.get('email'),
     message: state.app.get('message')
   }), {
-    hideMessage
+    goHome: () => push('/'),
+    hideMessage,
+    logout
   }
 )(
   App
