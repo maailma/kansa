c28681b9f2cebe551a203f5a979362bd2dd9c14f
Eemeli Aro <eemeli@gmail.com>
2016-09-10 18:58:52 +0300

    Remove hugo/member, as functionality transferred to client

--- hugo/member/src/components/App.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export default ({ title, children }) => <div>
-  <h1>{title}</h1>
-  {children}
-</div>;
--- hugo/member/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-import person from './person';
-import nominations from './nominations';
-
-export default combineReducers({ person, nominations });
-
