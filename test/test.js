445bde1e5c5e75c644ea630c6dfd09ae63c64de8
Juha Autero <jautero@iki.fi>
2016-09-11 12:02:04 +0300

    Improved descriptions and added retries

--- test/test.js
+++ test/test.js
@@ -3,26 +3,28 @@ var request = require('supertest');
 var fs = require('fs');
 var cert = fs.readFileSync('nginx/ssl/localhost.cert','utf8');
 
-describe("Check API points",function () {
-    it("Check that web server is up",function (done) {
+describe("Check that API services are up",function () {
+    this.retries(3); /* Then shalt thou count to three, no more, no less. Three shall be
+        the number thou shalt count, and the number of the counting shall
+        be three. Four shalt thou not count, neither count thou two,
+        excepting that thou then proceed to three. Five is right out. */
+    it("Should have web server running",function (done) {
         request.agent(host,{ca:cert})
             .get('/')
             .expect("Content-Type",/html/)
             .expect(200)
         .end(done);
     });
-    it("Check that api/kansa/ is routed", function (done) {
+    it("Should respond with json on api/kansa/", function (done) {
         request.agent(host,{ca:cert})
             .get("/api/kansa/")
             .expect("Content-Type",/json/)
-            .expect(401)
         .end(done);
     })
-    it("Check that api/hugo/ is routed", function (done) {
+    it("Should respond with json on api/hugo/", function (done) {
         request.agent(host,{ca:cert})
             .get("/api/hugo/")
             .expect("Content-Type",/json/)
-            .expect(404)
         .end(done);
     })
     
