3be67666cf2358f248f1f786e4e2c10acd15214a
Juha Autero <jautero@iki.fi>
2016-09-13 07:13:34 +0300

    Gave up trying to re-run tests and just added sleep.

--- api-testing/test/test.js
+++ api-testing/test/test.js
@@ -4,19 +4,6 @@ var fs = require('fs');
 var cert = fs.readFileSync('../nginx/ssl/localhost.cert','utf8');
 
 describe("Check that API services are up",function () {
-    this.retries(3); /* Then shalt thou count to three, no more, no less. Three shall be
-        the number thou shalt count, and the number of the counting shall
-        be three. Four shalt thou not count, neither count thou two,
-        excepting that thou then proceed to three. Five is right out. */
-    afterEach(function (done) { // wait for one second after failed test before retry.
-        if (this.currentTest.state === 'failed') {
-            setTimeout(function () {
-                done();
-            },1000);
-        } else {
-            done();
-        }
-    })
     it("Should have web server running",function (done) {
         request.agent(host,{ca:cert})
             .get('/')
