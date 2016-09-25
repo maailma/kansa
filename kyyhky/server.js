ab199540a5d0c72ef4be2cf1d843922f553f44aa
Eemeli Aro <eemeli@gmail.com>
2016-09-26 00:23:40 +0300

    Add hugo-update-nominations, with skipping of too-early messages

--- kyyhky/server.js
+++ kyyhky/server.js
@@ -1,10 +1,15 @@
 const kue = require('kue');
 const queue = kue.createQueue({
+  disableSearch: false,
   redis: {
     host: process.env.REDIS_HOST || 'redis',
     port: process.env.REDIS_PORT || 6379
   }
 });
+
+const reds = require('reds');  // using kue's dependency to guarantee version match
+reds.createClient = require('kue/lib/redis').createClient;
+const search = reds.createSearch(queue.client.getKey('search'));
 
 const Mailer = require('./lib/mailer');
 const mailer = new Mailer('templates', '.mustache', process.env.SENDGRID_APIKEY);
@@ -15,6 +20,8 @@ queue.on('job complete', (id, result) => {
       if (err) throw err;
       if (result && result.to) {
         console.log('Job #%d: Sent %s to %s', job.id, job.type, result.to);
+      } else if (job.data.email) {
+        console.log('Job #%d: Skipped %s for %s', job.id, job.type, job.data.email);
       }
     });
   });
@@ -26,6 +33,19 @@ queue.on('job failed', (id, result) => {
 
 queue.process('kansa-set-key', (job, done) => {
   mailer.sendEmail(job.type, job.data, done);
+});
+
+queue.process('hugo-update-nominations', (job, done) => {
+  const email = job.data.email;
+  search.query(email).end((err, ids) => {
+    if (err) return done(err);
+    const later = ids.filter(id => id > job.id);  // TODO: verify that this is valid
+    if (later.length > 0) {
+      done();
+    } else {
+      mailer.sendEmail(job.type, job.data, done);
+    }
+  });
 });
 
 kue.app.listen(3000);
