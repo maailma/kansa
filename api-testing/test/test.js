e3306fb05186883efd53733e3632594531f2bb69
Juha Autero <jautero@iki.fi>
2016-09-27 19:00:29 +0300

    Added test for stats and agents for not logged in and admin user.

--- api-testing/test/test.js
+++ api-testing/test/test.js
@@ -3,6 +3,12 @@ var request = require('supertest');
 var assert = require('assert');
 var fs = require('fs');
 var cert = fs.readFileSync('../nginx/ssl/localhost.cert','utf8');
+// For now test data is empty.
+var memberlist = [];
+var memberstats = {"":{"total":0}};
+// Create agent for unlogged and admin sessions
+var unlogged=request.agent(host,{ca:cert});
+var admin=request.agent(host,{ca:cert});
 
 describe("Check that API services are up",function () {
     this.retries(5);
@@ -14,33 +20,33 @@ describe("Check that API services are up",function () {
         }
     })
     it("Should have web server running",function (done) {
-        request.agent(host,{ca:cert})
-            .get('/')
+        unlogged.get('/')
             .expect("Content-Type",/html/)
             .expect(200)
         .end(done);
     });
     it("Should respond with json on api/kansa/", function (done) {
-        request.agent(host,{ca:cert})
-            .get("/api/kansa/")
+        unlogged.get("/api/kansa/")
             .expect("Content-Type",/json/)
         .end(done);
     })
     it("Should respond with json on api/hugo/", function (done) {
-        request.agent(host,{ca:cert})
-            .get("/api/hugo/")
+        unlogged.get("/api/hugo/")
             .expect("Content-Type",/json/)
         .end(done);
     })
 })
 
-describe("GET /api/kansa/public/people",function () {
-    context("With empty database", function () {
-        it ("Returns empty JSON list", function (done) {
-            request.agent(host,{ca:cert})
-            .get("/api/kansa/public/people")
-            .expect(200,{status: 'success', data: []})
+describe("Member list",function () {
+    it ("Returns 'success' as status and test data member list.", function (done) {
+        unlogged.get("/api/kansa/public/people")
+        .expect(200,{status: 'success', data: memberlist})
             .end(done);
         })
+})
+
+describe("Country statistics", function () {
+    it("Returns country statistics", function (done) {
+        unlogged.get("/api/kansa/public/stats").expect(200,{status:'success', members: memberstats}).end(done)
     })
 })
