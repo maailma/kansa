25f0be58672324202260b1b1af788e9a078d794b
Eemeli Aro <eemeli@gmail.com>
2016-10-01 15:59:56 +0300

    Add callback param to TRY_LOGIN action

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -13,7 +13,7 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 import { keyLogin, tryLogin } from './app/actions/auth'
 import { setNominator } from './hugo/actions'
-import { PATH_IN } from './constants'
+import { PATH_IN, PATH_OUT } from './constants'
 import App from './app/components/App'
 import LoginForm from './app/components/LoginForm'
 import MemberList from './kansa/components/MemberList'
@@ -25,21 +25,17 @@ import './app/style.css'
 
 const store = createStore(reducers, middleware(hashHistory));
 
-const authCheck = (nextState, replace) => {
-  const loc = nextState.location.pathname;
+const authCheck = (_, replace, callback) => {
   const email = store.getState().user.get('email');
-  if (!email) store.dispatch(tryLogin());
-  else if (loc == '/') replace(PATH_IN);
+  if (email) callback();
+  else store.dispatch(tryLogin(err => {
+    if (err) replace(PATH_OUT);
+    callback();
+  }));
 }
 
 const doLogin = ({ params: { email, key } }) => {
   store.dispatch(keyLogin(email, key));
-}
-
-const onEnterHugo = (nextState, replace) => {
-  const id = parseInt(nextState.params.id);
-  const person = store.getState().user.get('people', []).find(p => p.id === id);
-  if (!person) store.dispatch(tryLogin());
 }
 
 const onEnterNominations = ({ params: { id } }, _, callback) => {
@@ -57,7 +53,7 @@ ReactDOM.render(
           <Route path="profile" onEnter={authCheck} component={MemberList} />
           <Route path="hugo" onEnter={authCheck} >
             <IndexRedirect to={PATH_IN} />
-            <Route path=":id" onEnter={onEnterHugo} >
+            <Route path=":id">
               <IndexRedirect to="nominate" />
               <Route path="nominate" onEnter={onEnterNominations} component={Nominate} />
             </Route>
