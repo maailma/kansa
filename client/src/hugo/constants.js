c186520e6f94fc42ad06d4a6767bce302e30ff29
Eemeli Aro <eemeli@gmail.com>
2016-10-01 17:09:06 +0300

    Use categoryTexts data in nominationFields()

--- client/src/hugo/constants.js
+++ client/src/hugo/constants.js
@@ -4,38 +4,6 @@ export const categories = [ 'Novel', 'Novella', 'Novelette', 'ShortStory', 'Rela
 
 export const maxNominationsPerCategory = 5;
 
-export const nominationFields = (category) => { switch (category) {
-
-  case 'Novel':
-  case 'Novella':
-  case 'Novelette':
-  case 'ShortStory':
-  case 'RelatedWork':
-  case 'GraphicStory':
-    return [ 'author', 'title', 'publisher' ];
-
-  case 'DramaticLong':
-  case 'DramaticShort':
-    return [ 'title', 'set' ];
-
-  case 'EditorLong':
-  case 'EditorShort':
-    return [ 'editor' ];
-
-  case 'Semiprozine':
-  case 'Fanzine':
-  case 'Fancast':
-    return [ 'title' ];
-
-  case 'FanWriter':
-  case 'FanArtist':
-  case 'ProArtist':
-  case 'Campbell':
-    return [ 'author', 'example' ];
-
-  default: throw new Error('Unknown category ' + JSON.stringify(category));
-}}
-
 export const categoryTexts = {
   Novel: {
     title: 'Best Novel',
@@ -190,4 +158,10 @@ export const categoryTexts = {
       example: 'Example'
     }
   }
+}
+
+export const nominationFields = (category) => {
+  const texts = categoryTexts[category];
+  if (!texts) throw new Error('Unknown category ' + JSON.stringify(category));
+  return Object.keys(texts.nominationFieldLabels);
 }
