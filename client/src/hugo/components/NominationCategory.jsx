e2bae5611fdac31bea8cdbd97b951f8288a74295
Eemeli Aro <eemeli@gmail.com>
2016-10-02 01:20:47 +0300

    Split NominationRow.jsx from NominationCategory.jsx

--- client/src/hugo/components/NominationCategory.jsx
+++ client/src/hugo/components/NominationCategory.jsx
@@ -4,11 +4,9 @@ import ImmutablePropTypes from 'react-immutable-proptypes'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
-import IconButton from 'material-ui/IconButton'
 import Paper from 'material-ui/Paper'
 import RaisedButton from 'material-ui/RaisedButton'
 import ListCheck from 'material-ui/svg-icons/av/playlist-add-check'
-import ContentClear from 'material-ui/svg-icons/content/clear'
 import ContentUndo from 'material-ui/svg-icons/content/undo'
 import TextField from 'material-ui/TextField'
 
@@ -16,93 +14,8 @@ import time_diff from '../../lib/time_diff'
 import { editNomination, submitNominations, resetNominations } from '../actions'
 import { categoryInfo, maxNominationsPerCategory, nominationFields } from '../constants'
 
+import { NominationFillerRow, NominationRow } from './NominationRow'
 
-const NominationField = ({ changed, disabled, name, onChange, setRef, value, width }) => <TextField
-  className={ 'NominationField' + (changed ? ' changed' : '') }
-  disabled={disabled}
-  multiLine={true}
-  name={name}
-  onChange={onChange}
-  ref={setRef}
-  style={{ width }}
-  value={value}
-/>;
-
-NominationField.propTypes = {
-  changed: React.PropTypes.bool,
-  disabled: React.PropTypes.bool,
-  name: React.PropTypes.string.isRequired,
-  onChange: React.PropTypes.func,
-  value: React.PropTypes.string.isRequired
-}
-
-
-class NominationRemoveButton extends React.Component {
-  static propTypes = {
-    disabled: React.PropTypes.bool.isRequired,
-    onRemove: React.PropTypes.func
-  }
-
-  shouldComponentUpdate(nextProps) {
-    return nextProps.disabled !== this.props.disabled;
-  }
-
-  render() {
-    const { disabled, onRemove } = this.props;
-    return <IconButton
-      disabled={disabled}
-      onTouchTap={onRemove}
-      tooltip='Remove nomination'
-      tooltipStyles={{ top: 24 }}
-    >
-      <ContentClear />
-    </IconButton>;
-  }
-}
-
-
-class NominationRow extends React.Component {
-  static propTypes = {
-    defaultValues: ImmutablePropTypes.map.isRequired,
-    disabled: React.PropTypes.bool,
-    fields: React.PropTypes.array.isRequired,
-    onChange: React.PropTypes.func,
-    onRemove: React.PropTypes.func,
-    setLastField: React.PropTypes.func,
-    values: ImmutablePropTypes.map.isRequired,
-    width: React.PropTypes.number.isRequired
-  }
-
-  shouldComponentUpdate(nextProps) {
-    const { defaultValues, disabled, values } = this.props;
-    return nextProps.disabled !== disabled || !defaultValues.equals(nextProps.defaultValues) || !values.equals(nextProps.values);
-  }
-
-  render() {
-    const { defaultValues, disabled, fields, onChange, onRemove, setLastField, values, width } = this.props;
-    return <tr>
-      {
-        fields.map(field => <td
-          key={field}
-          className='NominationFieldCell'
-        >
-          <NominationField
-            changed={ values.get(field, '') != defaultValues.get(field, '') }
-            disabled={disabled}
-            name={field}
-            onChange={ ev => onChange(field, ev.target.value) }
-            setRef={ ref => { if (ref && values.isEmpty()) setLastField(field, ref); } }
-            value={ values.get(field, '') }
-            width={width}
-          />
-        </td>)
-      }
-      <td>
-        { values.isEmpty() ? null : <NominationRemoveButton disabled={disabled} onRemove={onRemove} /> }
-      </td>
-    </tr>;
-  }
-}
 
 
 class NominationActionsRow extends React.Component {
@@ -140,23 +53,10 @@ class NominationActionsRow extends React.Component {
   }
 }
 
-const nominationRowLinks = (n, fields, lastRow, width) => {
+const nominationRowLinks = (n, props) => {
   if (n <= 0) return null;
   const res = [];
-  for (let i = 0; i < n; ++i) res.push(<tr key={`link-${i}`}>
-    {
-      fields.map(field => <td key={field}>
-        <TextField
-          className="NominationField NominationLink"
-          name={field}
-          onFocus={ () => lastRow[field] && lastRow[field].focus() }
-          style={{ width }}
-          underlineFocusStyle={{ display: 'none' }}
-          value=""
-        />
-      </td>)
-    }
-  </tr>);
+  for (let i = 0; i < n; ++i) res.push(<NominationFillerRow key={`link-${i}`} {...props} />);
   return res;
 }
 
@@ -182,7 +82,7 @@ const NominationBody = ({ fields, maxNominations, onChange, onSave, onReset, sta
         width={width}
       />)
     }
-    { nominationRowLinks(maxNominations - rows.size, fields, lastRow, width) }
+    { nominationRowLinks(maxNominations - rows.size, { fields, lastRow, width }) }
     <NominationActionsRow
       colSpan={fields.length}
       disabled={ isFetching || clientData.equals(serverData) }
