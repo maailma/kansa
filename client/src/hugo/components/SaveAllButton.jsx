215411b263d0ffc1aa3f738e4e7e5736017a3580
Eemeli Aro <eemeli@gmail.com>
2016-10-08 22:15:36 +0300

    Add a counter Badge to the nominations SaveAllButton

--- client/src/hugo/components/SaveAllButton.jsx
+++ client/src/hugo/components/SaveAllButton.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+import Badge from 'material-ui/Badge'
 import IconButton from 'material-ui/IconButton'
 import Paper from 'material-ui/Paper'
 import ListCheck from 'material-ui/svg-icons/av/playlist-add-check'
@@ -19,29 +20,42 @@ const SaveAllButton = ({ changedCategories, submitNominations }) => (
     }}
     zDepth={2}
   >
-    <IconButton
-      disabled={ changedCategories.size == 0 }
-      onTouchTap={ () => changedCategories.keySeq().forEach(submitNominations) }
-      style={{
-        transition: transitions.easeOut(),
-        position: 'relative',
-        height: 56,
-        width: 56,
-        padding: 0,
-        borderRadius: '50%',
-        textAlign: 'center',
-        verticalAlign: 'bottom'
+    <Badge
+      badgeContent={changedCategories.size}
+      badgeStyle={{
+        fontWeight: 'bold',
+        pointerEvents: 'none',
+        right: -6,
+        top: -6,
+        zoom: 1.1
       }}
-      tooltip='Save all categories'
-      tooltipPosition='top-left'
-      tooltipStyles={{
-        fontSize: 12,
-        right: 64,
-        marginTop: 40
-      }}
+      primary={true}
+      style={{ padding: 0 }}
     >
-      <ListCheck />
-    </IconButton>
+      <IconButton
+        disabled={ changedCategories.size == 0 }
+        onTouchTap={ () => changedCategories.keySeq().forEach(submitNominations) }
+        style={{
+          transition: transitions.easeOut(),
+          position: 'relative',
+          height: 56,
+          width: 56,
+          padding: 0,
+          borderRadius: '50%',
+          textAlign: 'center',
+          verticalAlign: 'bottom'
+        }}
+        tooltip='Save all categories'
+        tooltipPosition='top-left'
+        tooltipStyles={{
+          fontSize: 12,
+          right: 64,
+          marginTop: 40
+        }}
+      >
+        <ListCheck />
+      </IconButton>
+    </Badge>
   </Paper>
 );
 
