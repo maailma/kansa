83961c83eba7f032723f5416e68722eccff632ac
Eemeli Aro <eemeli@gmail.com>
2016-10-15 12:15:02 +0300

    1980: Update text contents

--- client/src/1980/Intro.jsx
+++ client/src/1980/Intro.jsx
@@ -3,13 +3,18 @@ import React from 'react'
 export default ({ setShowMore, showMore }) => <div>
 
   <p>
-    Next year’s Hugo nominations will be the most complex ever, with six finalists for each award, a new tallying
-    system, and a special category for Best Series. We are developing new software solutions for the Hugos – and we
-    need to test them.
+    Next year’s Hugo nominations will be the most complex ever, with six finalists for each award, a
+    new tallying system, and a special category for Best Series. We are developing new software
+    solutions for the Hugos – and we need to test them.
   </p>
   <p>
-    We are therefore looking back as well as looking forward – and we are inviting fans to think themselves back to
-    1979, and the SF and Fantasy of that year, for the purpose of giving us fresh data to test the software with.
+    We are therefore looking back as well as looking forward – and we are inviting Worldcon 75 staff
+    and friends to think themselves back to 1979, and the SF and Fantasy of that year, for the purpose
+    of giving us fresh data to test the software with.
+  </p>
+  <p>
+    We are not making this into a big public process, but at the same time feel free to invite friends
+    who are not on Worldcon 75 staff to participate.
   </p>
 
   { showMore ? <div>
@@ -17,30 +22,36 @@ export default ({ setShowMore, showMore }) => <div>
     <h2>Why 1980?</h2>
 
     <p>
-      This was, frankly, a fairly arbitrary decision. We wanted to choose a year which was not too far back in the
-      mists of time, but also not so recent as to reopen unnecessary controversy.
+      This was, frankly, a fairly arbitrary decision. We wanted to choose a year which was not too far
+      back in the mists of time, but also not so recent as to reopen unnecessary controversy. By
+      fortunate coincidence, the full counting details from 1980, including the long lists, have been <a
+      href="http://smofinfo.com/wsfs/Hugos/1980%20--%20Hugo%20voting%20details.pdf">preserved</a> so we
+      can see what fans at the time nominated. (Of course there were different categories then - the
+      soon-to-be-abandoned Gandalf Award, only one Dramatic Presentation category, only one Professional
+      Editor category, “Non-Fiction” rather than “Related Work”, no Graphic Story, no Semiprozine, no
+      Fancast, no Series.)
     </p>
     <p>
-      The 1980 Timewarp Project obviously doesn’t replace or in any way invalidate the real historic 1980 Hugo
-      nominations, final ballot or winners – it just helps the Worldcon 75 Hugo administrators test the systems in
-      advance of the 2017 nominations and vote. The <a
-      href="http://www.thehugoawards.org/hugo-history/1980-hugo-awards/">Hugo Awards for 1980</a> were presented at
-      Noreascon Two in Boston; those rewards remain part of history and will never be changed.
+      The 1980 Timewarp Project obviously doesn’t replace or in any way invalidate the real historic 1980
+      Hugo nominations, final ballot or winners – it just helps the Worldcon 75 Hugo administrators test
+      the systems in advance of the 2017 nominations and vote. The <a
+      href="http://www.thehugoawards.org/hugo-history/1980-hugo-awards/">Hugo Awards for 1980</a> were
+      presented at Noreascon Two in Boston; those rewards remain part of history and will never be
+      changed.
     </p>
 
     <h2>So what are you doing?</h2>
 
     <p>
-      From 9 to 26 October, fans are invited to make nominations for the Hugo categories under the rules for 2017,
-      but with respect to the state of the genre and fandom of 1979. Anyone can nominate, but only electronic
-      nominations will be accepted.
+      From 15 to 31 October, you are invited to make nominations for the Hugo categories under the rules
+      for 2017, but with respect to the state of the genre and fandom of 1979. Anyone can nominate, but
+      only electronic nominations will be accepted.
     </p>
     <p>
-      We will publish the full dataset of submitted nominations and the final Timewarp Ballot that would have
-      emerged from those nominations if the current rules had been in force in 1980. We will not then proceed to any
-      further vote – the Timewarp Ballot, and the dataset of nominations, are the end of the process. The released
-      dataset will be stripped of all personally identifying data. Email addresses used to register for the project
-      will be deleted from our systems at its conclusion.
+      We will publish the full dataset of submitted nominations (though without submitters’ names
+      attached) and the final Timewarp Ballot that would have emerged from those nominations if the
+      current rules had been in force in 1980. We will not then proceed to any further vote – the
+      Timewarp Ballot, and the dataset of nominations, are the end of the process.
     </p>
 
   </div> : <a
--- client/src/hugo/constants.js
+++ client/src/hugo/constants.js
@@ -3,7 +3,7 @@ export const maxNominationsPerCategory = 5;
 export const categoryInfo = {
   Novel: {
     title: 'Best Novel',
-    description: `If you had been nominating in 1980, what science fiction or fantasy story or stories of forty thousand (40,000) words or more, published for the first time in 1979, might you have nominated?`,
+    description: `If you had been nominating for the Hugos in 1980, what science fiction or fantasy story or stories of forty thousand (40,000) words or more, published for the first time in 1979, might you have nominated?`,
     nominationFieldLabels: {
       author: 'Author',
       title: 'Title',
@@ -13,7 +13,7 @@ export const categoryInfo = {
 
   Novella: {
     title: 'Best Novella',
-    description: `If you had been nominating in 1980, what science fiction or fantasy story or stories of between seventeen thousand five hundred (17,500) and forty thousand (40,000) words, published for the first time in 1979, might you have nominated?`,
+    description: `If you had been nominating for the Hugos in 1980, what science fiction or fantasy story or stories of between seventeen thousand five hundred (17,500) and forty thousand (40,000) words, published for the first time in 1979, might you have nominated?`,
     nominationFieldLabels: {
       author: 'Author',
       title: 'Title',
@@ -23,7 +23,7 @@ export const categoryInfo = {
 
   Novelette: {
     title: 'Best Novelette',
-    description: `If you had been nominating in 1980, what science fiction or fantasy story or stories of between seven thousand five hundred (7,500) and seventeen thousand five hundred (17,500) words, published for the first time in 1979, might you have nominated?`,
+    description: `If you had been nominating for the Hugos in 1980, what science fiction or fantasy story or stories of between seven thousand five hundred (7,500) and seventeen thousand five hundred (17,500) words, published for the first time in 1979, might you have nominated?`,
     nominationFieldLabels: {
       author: 'Author',
       title: 'Title',
@@ -33,7 +33,7 @@ export const categoryInfo = {
 
   ShortStory: {
     title: 'Best Short Story',
-    description: `If you had been nominating in 1980, what science fiction or fantasy story or stories of less than seven thousand five hundred (7,500) words, published for the first time in 1979, might you have nominated?`,
+    description: `If you had been nominating for the Hugos in 1980, what science fiction or fantasy story or stories of less than seven thousand five hundred (7,500) words, published for the first time in 1979, might you have nominated?`,
     nominationFieldLabels: {
       author: 'Author',
       title: 'Title',
@@ -43,7 +43,7 @@ export const categoryInfo = {
 
   RelatedWork: {
     title: 'Best Related Work',
-    description: `If you had been nominating in 1980, what work or works related to the field of science fiction, fantasy, or fandom, appearing for the first time or substantially modified in 1979, and either non-fiction or, if fictional, noteworthy primarily for aspects other than the fictional text, and also not eligible in any other category, might you have nominated?`,
+    description: `If you had been nominating for the Hugos in 1980, what work or works related to the field of science fiction, fantasy, or fandom, appearing for the first time or substantially modified in 1979, and either non-fiction or, if fictional, noteworthy primarily for aspects other than the fictional text, and also not eligible in any other category, might you have nominated?`,
     nominationFieldLabels: {
       author: 'Author/Editor',
       title: 'Title',
@@ -53,7 +53,7 @@ export const categoryInfo = {
 
   GraphicStory: {
     title: 'Best Graphic Story',
-    description: `If you had been nominating in 1980, what science fiction or fantasy story or stories told in graphic form, published for the first time in 1979, might you have nominated?`,
+    description: `If you had been nominating for the Hugos in 1980, what science fiction or fantasy story or stories told in graphic form, published for the first time in 1979, might you have nominated?`,
     nominationFieldLabels: {
       author: 'Author',
       title: 'Title',
@@ -63,7 +63,7 @@ export const categoryInfo = {
 
   DramaticLong: {
     title: 'Best Dramatic Presentation, Long Form',
-    description: `If you had been nominating in 1980, what theatrical feature(s) or other production(s), with a complete running time of more than 90 minutes, in any medium of dramatized science fiction, fantasy or related subjects, publicly presented for the first time in that form in 1979, might you have nominated?`,
+    description: `If you had been nominating for the Hugos in 1980, what theatrical feature(s) or other production(s), with a complete running time of more than 90 minutes, in any medium of dramatized science fiction, fantasy or related subjects, publicly presented for the first time in that form in 1979, might you have nominated?`,
     nominationFieldLabels: {
       title: 'Title',
       producer: 'Studio/Network/Production Company'
@@ -72,7 +72,7 @@ export const categoryInfo = {
 
   DramaticShort: {
     title: 'Best Dramatic Presentation, Short Form',
-    description: `If you had been nominating in 1980, what theatrical feature(s) or other production(s), with a complete running time of 90 minutes or less, in any medium of dramatized science fiction, fantasy or related subjects, publicly presented for the first time in that form in 1979, might you have nominated?`,
+    description: `If you had been nominating for the Hugos in 1980, what theatrical feature(s) or other production(s), with a complete running time of 90 minutes or less, in any medium of dramatized science fiction, fantasy or related subjects, publicly presented for the first time in that form in 1979, might you have nominated?`,
     nominationFieldLabels: {
       title: 'Title',
       series: '(Series)',
@@ -82,7 +82,7 @@ export const categoryInfo = {
 
   EditorShort: {
     title: 'Best Professional Editor, Short Form',
-    description: `If you had been nominating in 1980, which editor or editors of at least four (4) anthologies, collections or magazine issues (or their equivalent in other media) primarily devoted to science fiction and / or fantasy, at least one of which was published in 1979, might you have nominated?`,
+    description: `If you had been nominating for the Hugos in 1980, which editor or editors of at least four (4) anthologies, collections or magazine issues (or their equivalent in other media) primarily devoted to science fiction and / or fantasy, at least one of which was published in 1979, might you have nominated?`,
     nominationFieldLabels: {
       editor: 'Editor'
     }
@@ -90,7 +90,7 @@ export const categoryInfo = {
 
   EditorLong: {
     title: 'Best Professional Editor, Long Form',
-    description: `If you had been nominating in 1980, which editor or editors of at least four (4) novel-length works (that do not qualify as works under Best Editor, Short Form) primarily devoted to science fiction and / or fantasy published in 1979 might you have nominated?`,
+    description: `If you had been nominating for the Hugos in 1980, which editor or editors of at least four (4) novel-length works (that do not qualify as works under Best Editor, Short Form) primarily devoted to science fiction and / or fantasy published in 1979 might you have nominated?`,
     nominationFieldLabels: {
       editor: 'Editor'
     }
@@ -98,7 +98,7 @@ export const categoryInfo = {
 
   ProArtist: {
     title: 'Best Professional Artist',
-    description: `If you had been nominating in 1980, which illustrator or illustrators, whose work had appeared in a professional publication in the field of science fiction or fantasy in 1979, might you have nominated?`,
+    description: `If you had been nominating for the Hugos in 1980, which illustrator or illustrators, whose work had appeared in a professional publication in the field of science fiction or fantasy in 1979, might you have nominated?`,
     nominationFieldLabels: {
       author: 'Artist/Illustrator',
       example: 'Example'
@@ -107,7 +107,7 @@ export const categoryInfo = {
 
   Semiprozine: {
     title: 'Best Semiprozine',
-    description: `If you had been nominating in 1980, what generally available non-professional periodical publication or publications, devoted to science fiction or fantasy, or related subjects which by 1979 had published four (4) or more issues (or the equivalent in other media), at least one (1) of which appeared in 1979, which did not qualify as a fancast, and which in 1979 met at least one (1) of the following criteria: (1) paid its contributors and/or staff in other than copies of the publication, (2) was generally available only for paid purchase, might you have nominated?`,
+    description: `If you had been nominating for the Hugos in 1980, what generally available non-professional periodical publication or publications, devoted to science fiction or fantasy, or related subjects which by 1979 had published four (4) or more issues (or the equivalent in other media), at least one (1) of which appeared in 1979, which did not qualify as a fancast, and which in 1979 met at least one (1) of the following criteria: (1) paid its contributors and/or staff in other than copies of the publication, (2) was generally available only for paid purchase, might you have nominated?`,
     nominationFieldLabels: {
       title: 'Title'
     }
@@ -115,7 +115,7 @@ export const categoryInfo = {
 
   Fanzine: {
     title: 'Best Fanzine',
-    description: `If you had been nominating in 1980, what generally available non-professional periodical publication or publications, devoted to science fiction, fantasy, or related subjects that by 1979 had published four (4) or more issues (or the equivalent in other media), at least one (1) of which appeared in 1979, that did not qualify as a semiprozine or a fancast, and that in 1979 met neither of the following criteria: (1) paid its contributors or staff monetarily in other than copies of the publication, (2) was generally available only for paid purchase, might you have nominated?`,
+    description: `If you had been nominating for the Hugos in 1980, what generally available non-professional periodical publication or publications, devoted to science fiction, fantasy, or related subjects that by 1979 had published four (4) or more issues (or the equivalent in other media), at least one (1) of which appeared in 1979, that did not qualify as a semiprozine or a fancast, and that in 1979 met neither of the following criteria: (1) paid its contributors or staff monetarily in other than copies of the publication, (2) was generally available only for paid purchase, might you have nominated?`,
     nominationFieldLabels: {
       title: 'Title'
     }
@@ -123,7 +123,7 @@ export const categoryInfo = {
 
   Fancast: {
     title: 'Best Fancast',
-    description: `If you had been nominating in 1980, what generally available non-professional audio or video periodical or periodicals devoted to science fiction, fantasy, or related subjects that by 1979 had released four (4) or more episodes, at least one (1) of which appeared in 1979, and that did not qualify as a dramatic presentation, might you have nominated?`,
+    description: `If you had been nominating for the Hugos in 1980, what generally available non-professional audio or video periodical or periodicals devoted to science fiction, fantasy, or related subjects that by 1979 had released four (4) or more episodes, at least one (1) of which appeared in 1979, and that did not qualify as a dramatic presentation, might you have nominated?`,
     nominationFieldLabels: {
       title: 'Title'
     }
@@ -131,7 +131,7 @@ export const categoryInfo = {
 
   FanWriter: {
     title: 'Best Fan Writer',
-    description: `If you had been nominating in 1980, which person or persons whose writing appeared in semiprozines or fanzines or in generally available electronic media during 1979 might you have nominated?`,
+    description: `If you had been nominating for the Hugos in 1980, which person or persons whose writing appeared in semiprozines or fanzines or in generally available electronic media during 1979 might you have nominated?`,
     nominationFieldLabels: {
       author: 'Author',
       example: 'Example'
@@ -140,7 +140,7 @@ export const categoryInfo = {
 
   FanArtist: {
     title: 'Best Fan Artist',
-    description: `If you had been nominating in 1980, which artist(s) and/or cartoonist(s) whose work appeared through publication in semiprozines or fanzines or through other public, non-professional, display (including at a convention or conventions) during 1979 might you have nominated?`,
+    description: `If you had been nominating for the Hugos in 1980, which artist(s) and/or cartoonist(s) whose work appeared through publication in semiprozines or fanzines or through other public, non-professional, display (including at a convention or conventions) during 1979 might you have nominated?`,
     nominationFieldLabels: {
       author: 'Artist/Illustrator',
       example: 'Example'
@@ -149,7 +149,7 @@ export const categoryInfo = {
 
   Series: {
     title: 'Best Series',
-    description: `If you had been nominating in 1980, which multi-volume science fiction or fantasy story or stories, unified by elements such as plot, characters, setting, and presentation, which had appeared in at least three (3) volumes consisting of a total of at least 240,000 words by 1979, at least one of which was published in 1979, might you have nominated?`,
+    description: `If you had been nominating for the Hugos in 1980, which multi-volume science fiction or fantasy story or stories, unified by elements such as plot, characters, setting, and presentation, which had appeared in at least three (3) volumes consisting of a total of at least 240,000 words by 1979, at least one of which was published in 1979, might you have nominated?`,
     nominationFieldLabels: {
       author: 'Author',
       title: 'Title',
@@ -158,8 +158,8 @@ export const categoryInfo = {
   },
 
   NewWriter: {
-    title: 'John W. Campbell Award (not a Hugo)',
-    description: `If you had been nominating in 1980, which new writer or writers whose first work of science fiction or fantasy appeared in 1978 or 1979 in a professional publication might you have nominated?`,
+    title: 'Best New Writer',
+    description: `If you had been nominating for the John W. Campbell Award for Best New Writer in 1980, which new writer or writers whose first work of science fiction or fantasy appeared in 1978 or 1979 in a professional publication might you have nominated?`,
     nominationFieldLabels: {
       author: 'Author',
       example: 'Example'
