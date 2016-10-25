46f1677799146287626a7e9e4b5e499190f728d9
Eemeli Aro <eemeli@gmail.com>
2016-10-25 20:36:04 +0300

    Move init & auth check from route to Canon ctor & cWRP

--- client/src/hugo-admin/components/Canon.jsx
+++ client/src/hugo-admin/components/Canon.jsx
@@ -3,36 +3,57 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import { nominationFields } from '../../hugo/constants'
+import { initHugoAdmin } from '../actions'
 import CanonNominationList from './CanonNominationList'
 
 class Canon extends React.Component {
+
   static propTypes = {
     canon: React.PropTypes.instanceOf(Map).isRequired,
     category: React.PropTypes.string.isRequired,
-    nominations: React.PropTypes.instanceOf(List).isRequired,
+    initHugoAdmin: React.PropTypes.func.isRequired,
+    isAdmin: React.PropTypes.bool.isRequired,
+    nominations: React.PropTypes.instanceOf(List)
+  }
+
+  constructor(props) {
+    super(props);
+    const { initHugoAdmin, isAdmin } = props;
+    if (isAdmin) initHugoAdmin();
+    this.state = {
+      selected: List()
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { initHugoAdmin, isAdmin, nominations } = nextProps;
+    if (!nominations && isAdmin) initHugoAdmin();
   }
 
   render() {
-    const { canon, category, nominations } = this.props;
-    return (
-      <div style={{ display: 'flex', height: 'calc(100vh - 48px)' }}>
-        <CanonNominationList
-          canon={canon}
-          fields={nominationFields(category)}
-          nominations={nominations}
-        />
-      </div>
-    )
+    const { canon, category, isAdmin, nominations } = this.props;
+    return <div
+      style={{ display: 'flex', height: 'calc(100vh - 56px - 48px)' }}
+    >{
+      isAdmin && nominations ? <CanonNominationList
+        canon={canon}
+        fields={nominationFields(category)}
+        nominations={nominations}
+      /> : isAdmin ? 'Loading...' : 'Admin rights required'
+    }</div>
   }
 }
 
 export default connect(
-  ({ hugoAdmin }) => {
+  ({ hugoAdmin, user }) => {
     const category = hugoAdmin.get('category');
     return {
       canon: hugoAdmin.getIn(['canon', category]) || Map(),
       category,
-      nominations: hugoAdmin.getIn(['nominations', category]) || List()
+      isAdmin: user.get('hugoAdmin', false),
+      nominations: hugoAdmin.getIn(['nominations', category])
     }
+  }, {
+    initHugoAdmin
   }
 )(Canon);
