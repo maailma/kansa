66ce8d0fc83941eae94cb6d47a813769a653cafb
Kimmo <kimmo@netzionale.com>
2016-11-11 12:43:39 +0200

    sync queries with api WIP

--- raami/queries.js
+++ raami/queries.js
@@ -6,9 +6,7 @@ var options = {
 };
 
 var pgp = require('pg-promise')(options);
-var connectionString = 'postgres://localhost:5432/api';
-var db = pgp(connectionString);
-// var db = pgp(process.env.DATABASE_URL)
+var db = pgp(process.env.DATABASE_URL)
 
 // add query functions
 
@@ -25,13 +23,11 @@ module.exports = {
 };
 
 function getArtists(req, res, next) {
-  db.any('select * from Artist')
+  db.any('select id,continent from Artist')
     .then(function (data) {
       res.status(200)
         .json({
-          status: 'success',
           data: data,
-          message: 'Retrieved ALL puppies'
         });
     })
     .catch(function (err) {
@@ -40,14 +36,12 @@ function getArtists(req, res, next) {
 }
 
 function getArtist(req, res, next) {
-  var member_id = parseInt(req.params.id);
-  db.one('select * from Artist where member_id = $1', member_id)
+  var person_id = parseInt(req.params.id);
+  db.one('select * from Artist where person_id = $1', person_id)
     .then(function (data) {
       res.status(200)
         .json({
-          status: 'success',
           data: data,
-          message: 'Retrieved ONE Artist'
         });
     })
     .catch(function (err) {
@@ -58,14 +52,15 @@ function getArtist(req, res, next) {
 
 function createArtist(req, res, next) {
   req.body.age = parseInt(req.body.age);
-  db.none('insert into Artist(name, breed, age, sex)' +
-      'values(${name}, ${breed}, ${age}, ${sex})',
+  // continent, url, filename, portfolio, category, orientation, description, transport
+  db.none('insert into Artist(continent, url, filename, portfolio, category, orientation, description, transport)' +
+      'values(${continent}, ${url}, ${filename}, ${portfolio},${category}, ${orientation},${description},${transport})',
     req.body)
     .then(function () {
       res.status(200)
         .json({
           status: 'success',
-          message: 'Inserted one puppy'
+          //inserted: id
         });
     })
     .catch(function (err) {
@@ -75,14 +70,22 @@ function createArtist(req, res, next) {
 
 
 function updateArtist(req, res, next) {
-  db.none('update Artists set name=$1, breed=$2, age=$3, sex=$4 where id=$5',
-    [req.body.name, req.body.breed, parseInt(req.body.age),
-      req.body.sex, parseInt(req.params.id)])
+  db.none('update Artists set continent=$1, url=$2, filename=$3, portfolio=$4, category=$5, orientation=$6, description=$7, transport=$8 where id=$0',
+    [req.params.id, 
+     req.body.continent, 
+     req.body.url,
+     req.body.filename,
+     req.body.portfolio,
+     req.body.category,
+     req.body.orientation,
+     req.body.description,
+     req.body.transport
+     ])
     .then(function () {
       res.status(200)
         .json({
           status: 'success',
-          message: 'Updated puppy'
+          updated: req.body
         });
     })
     .catch(function (err) {
@@ -90,33 +93,32 @@ function updateArtist(req, res, next) {
     });
 }
 
-function removeArtists(req, res, next) {
-  var _id = parseInt(req.params.id);
-  db.result('delete from Artist where id = $1', _id)
-    .then(function (result) {
-      /* jshint ignore:start */
-      res.status(200)
-        .json({
-          status: 'success',
-          message: `Removed ${result.rowCount} puppy`
-        });
-      /* jshint ignore:end */
-    })
-    .catch(function (err) {
-      return next(err);
-    });
-}
+// function removeArtists(req, res, next) {
+//   var _id = parseInt(req.params.id);
+//   db.result('delete from Artist where id = $1', _id)
+//     .then(function (result) {
+//       /* jshint ignore:start */
+//       res.status(200)
+//         .json({
+//           status: 'success',
+//           message: `Removed ${result.rowCount} puppy`
+//         });
+//       /* jshint ignore:end */
+//     })
+//     .catch(function (err) {
+//       return next(err);
+//     });
+// }
 
 /**** WORKS ***/
 
 function getWorks(req, res, next) {
-  db.any('select * from Works where artist_id = $1', artist_id)
+  db.any('select id from Works where artist_id = $1', artist_id)
     .then(function (data) {
       res.status(200)
         .json({
           status: 'success',
           data: data,
-          message: 'Retrieved ALL Works'
         });
     })
     .catch(function (err) {
@@ -132,7 +134,6 @@ function getWork(req, res, next) {
         .json({
           status: 'success',
           data: data,
-          message: 'Retrieved ONE Work'
         });
     })
     .catch(function (err) {
@@ -142,15 +143,14 @@ function getWork(req, res, next) {
 
 
 function createWork(req, res, next) {
-  req.body.age = parseInt(req.body.age);
-  db.none('insert into Works(name, breed, age, sex)' +
-      'values(${name}, ${breed}, ${age}, ${sex})',
+  db.none('insert into Works(`artist_id`, `title` , `width` , `height` , `technique` , `graduation` , `filename` , `image` , `price` )' +
+      'values(${aritst_id}, ${title}, ${width}, ${height}, ${technique}, ${graduation}, ${filename}, ${image}, ${price})',
     req.body)
     .then(function () {
       res.status(200)
         .json({
           status: 'success',
-          message: 'Inserted one Work'
+          // inserted: id,
         });
     })
     .catch(function (err) {
@@ -160,14 +160,13 @@ function createWork(req, res, next) {
 
 
 function updateWork(req, res, next) {
-  db.none('update Works set name=$1, breed=$2, age=$3, sex=$4 where id=$5',
-    [req.body.name, req.body.breed, parseInt(req.body.age),
-      req.body.sex, parseInt(req.params.id)])
+  db.none('update Works set artist_id=$1, title=$2, width=$3, height=$4, technique=$5, graduation=$6, filename=$7, image=$8, price=$9 where id=$0',
+    [parseInt(req.params.id), req.body.title, req.body.width, req.body.height, req.body.technique, req.body.graduation, req.body.filename, req.body.image, req.body.price])
     .then(function () {
       res.status(200)
         .json({
           status: 'success',
-          message: 'Updated Work'
+          updated: req.body
         });
     })
     .catch(function (err) {
@@ -183,7 +182,7 @@ function removeWork(req, res, next) {
       res.status(200)
         .json({
           status: 'success',
-          message: 'Removed ${result.rowCount} Work'
+          deleted: [_id]
         });
       /* jshint ignore:end */
     })
