6666f1ce8308194052282af1cc40cff827e9b21d
Eemeli Aro <eemeli@gmail.com>
2016-11-29 00:58:09 +0200

    integration-tests: Add upgrade purchase tests, using Stripe API

--- integration-tests/test/purchase.spec.js
+++ integration-tests/test/purchase.spec.js
@@ -1,10 +1,12 @@
 const request = require('supertest');
 const assert = require('assert');
 const fs = require('fs');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_APIKEY || 'sk_test_zq022Drx7npYPVEtXAVMaOJT');
 
 const cert = fs.readFileSync('../nginx/ssl/localhost.cert', 'utf8');
 const prices = require('../../kansa/server/static/prices.json');
 const host = 'https://localhost:4430';
+const adminLoginParams = { email: 'admin@example.com', key: 'key' };
 
 describe('Purchase', () => {
   const agent = request.agent(host, { ca: cert });
@@ -42,5 +44,52 @@ describe('Purchase', () => {
         })
         .end(done);
     });
+  });
+
+  context('Upgrades (using Stripe API)', function() {
+    this.timeout(10000);
+    const admin = request.agent(host, { ca: cert });
+    const agent = request.agent(host, { ca: cert });
+    const testName = 'test-' + (Math.random().toString(36)+'00000000000000000').slice(2, 7);
+    let testId;
+
+    before((done) => {
+      admin.get('/api/kansa/login')
+        .query(adminLoginParams)
+        .end(() => {
+          admin.post('/api/kansa/people')
+            .send({ membership: 'Supporter', email: 'test@example.com', legal_name: testName })
+            .expect((res) => {
+              if (res.status !== 200) throw new Error(`Member init failed! ${JSON.stringify(res.body)}`);
+              testId = res.body.id;
+            })
+            .end(done);
+        });
+    });
+
+    it('should apply an upgrade', (done) => {
+      stripe.tokens.create({
+        card: {
+          number: '4242424242424242',
+          exp_month: 12,
+          exp_year: 2020,
+          cvc: '123'
+        }
+      }).then(testToken => {
+        agent.post('/api/kansa/purchase')
+          .send({
+            amount: prices.Adult.amount - prices.Supporter.amount,
+            email: 'test@example.com',
+            token: testToken.id,
+            upgrades: [{ id: testId, membership: 'Adult' }]
+          })
+          .expect((res) => {
+            if (res.status !== 200) throw new Error(`Upgrade failed! ${JSON.stringify(res.body)}`);
+            // HERE
+          })
+          .end(done);
+      });
+    });
+
   });
 });
