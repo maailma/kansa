f6aff56c0da38353b46a7c3bc8bedccba8299081
Kimmo <kimmo@netzionale.com>
2016-12-14 14:37:24 +0200

    allow cors for debug

--- raami/routes/index.js
+++ raami/routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+// allow cors for debug reasons
 router.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
  });
