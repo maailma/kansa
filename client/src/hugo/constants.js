a76b6b272cbdf06f3ce06e4570d4324911700760
Eemeli Aro <eemeli@gmail.com>
2016-12-16 00:41:40 +0200

    Update Hugo nomination texts again

--- client/src/hugo/constants.js
+++ client/src/hugo/constants.js
@@ -1,9 +1,11 @@
+﻿const YEAR = 2016;
+
 export const maxNominationsPerCategory = 5;
 
 export const categoryInfo = {
   Novel: {
     title: 'Best Novel',
-    description: `A science fiction or fantasy story of forty thousand (40,000) words or more.`,
+    description: `A science fiction or fantasy story of forty thousand (40,000) words or more published in ${YEAR}.`,
     nominationFieldLabels: {
       title: 'Title',
       author: 'Author',
@@ -13,7 +15,7 @@ export const categoryInfo = {
 
   Novella: {
     title: 'Best Novella',
-    description: `A science fiction or fantasy story of between seventeen thousand five hundred (17,500) and forty thousand (40,000) words.`,
+    description: `A science fiction or fantasy story of between seventeen thousand five hundred (17,500) and forty thousand (40,000) words published in ${YEAR}.`,
     nominationFieldLabels: {
       title: 'Title',
       author: 'Author',
@@ -23,7 +25,7 @@ export const categoryInfo = {
 
   Novelette: {
     title: 'Best Novelette',
-    description: `A science fiction or fantasy story of between seven thousand five hundred (7,500) and seventeen thousand five hundred (17,500) words.`,
+    description: `A science fiction or fantasy story of between seven thousand five hundred (7,500) and seventeen thousand five hundred (17,500) words published in ${YEAR}.`,
     nominationFieldLabels: {
       title: 'Title',
       author: 'Author',
@@ -33,7 +35,7 @@ export const categoryInfo = {
 
   ShortStory: {
     title: 'Best Short Story',
-    description: `A science fiction or fantasy story of less than seven thousand five hundred (7,500) words.`,
+    description: `A science fiction or fantasy story of less than seven thousand five hundred (7,500) words published in ${YEAR}.`,
     nominationFieldLabels: {
       title: 'Title',
       author: 'Author',
@@ -43,7 +45,7 @@ export const categoryInfo = {
 
   RelatedWork: {
     title: 'Best Related Work',
-    description: `Any work related to the field of science fiction, fantasy, or fandom, appearing for the first time during the previous calendar year or which has been substantially modified during the previous calendar year, and which is either non-fiction or, if fictional, is noteworthy primarily for aspects other than the fictional text, and which is not eligible in any other category.`,
+    description: `Any work related to the field of science fiction, fantasy, or fandom, appearing for the first time during ${YEAR} or which has been substantially modified during ${YEAR}, and which is either non-fiction or, if fictional, is noteworthy primarily for aspects other than the fictional text, and which is not eligible in any other category.`,
     nominationFieldLabels: {
       title: 'Title',
       author: 'Author/Editor',
@@ -53,7 +55,7 @@ export const categoryInfo = {
 
   GraphicStory: {
     title: 'Best Graphic Story',
-    description: `Any science fiction or fantasy story told in graphic form appearing for the first time in the previous calendar year.`,
+    description: `Any science fiction or fantasy story told in graphic form appearing for the first time in ${YEAR}.`,
     nominationFieldLabels: {
       title: 'Title',
       author: 'Author',
@@ -63,7 +65,7 @@ export const categoryInfo = {
 
   DramaticLong: {
     title: 'Best Dramatic Presentation, Long Form',
-    description: `Any theatrical feature or other production, with a complete running time of more than 90 minutes, in any medium of dramatized science fiction, fantasy or related subjects that has been publicly presented for the first time in its present dramatic form during the previous calendar year.`,
+    description: `Any theatrical feature or other production, with a complete running time of more than 90 minutes, in any medium of dramatized science fiction, fantasy or related subjects that has been publicly presented for the first time in its present dramatic form during ${YEAR}.`,
     nominationFieldLabels: {
       title: 'Title',
       producer: 'Studio/Network/Production Company'
@@ -72,7 +74,7 @@ export const categoryInfo = {
 
   DramaticShort: {
     title: 'Best Dramatic Presentation, Short Form',
-    description: `Any television program or other production, with a complete running time of 90 minutes or less, in any medium of dramatized science fiction, fantasy or related subjects that has been publicly presented for the first time in its present dramatic form during the previous calendar year.`,
+    description: `Any television program or other production, with a complete running time of 90 minutes or less, in any medium of dramatized science fiction, fantasy or related subjects that has been publicly presented for the first time in its present dramatic form during ${YEAR}.`,
     nominationFieldLabels: {
       title: 'Title',
       series: '(Series)',
@@ -82,7 +84,7 @@ export const categoryInfo = {
 
   EditorShort: {
     title: 'Best Professional Editor, Short Form',
-    description: `The editor of at least four (4) anthologies, collections or magazine issues (or their equivalent in other media) primarily devoted to science fiction and / or fantasy, at least one of which was published in the previous calendar year.`,
+    description: `The editor of at least four (4) anthologies, collections or magazine issues (or their equivalent in other media) primarily devoted to science fiction and / or fantasy, at least one of which was published in ${YEAR}.`,
     nominationFieldLabels: {
       editor: 'Editor'
     }
@@ -90,7 +92,7 @@ export const categoryInfo = {
 
   EditorLong: {
     title: 'Best Professional Editor, Long Form',
-    description: `The editor of at least four (4) novel-length works primarily devoted to science fiction and / or fantasy published in the previous calendar year that do not qualify as works under Best Editor, Shot Form.`,
+    description: `The editor of at least four (4) novel-length works primarily devoted to science fiction and / or fantasy published in ${YEAR} that do not qualify as works under Best Editor, Short Form.`,
     nominationFieldLabels: {
       editor: 'Editor'
     }
@@ -98,7 +100,7 @@ export const categoryInfo = {
 
   ProArtist: {
     title: 'Best Professional Artist',
-    description: `An illustrator whose work has appeared in a professional publication in the field of science fiction or fantasy during the previous calendar year.`,
+    description: `An illustrator whose work has appeared in a professional publication in the field of science fiction or fantasy during ${YEAR}.`,
     nominationFieldLabels: {
       author: 'Artist/Illustrator',
       example: 'Example'
@@ -107,7 +109,7 @@ export const categoryInfo = {
 
   Semiprozine: {
     title: 'Best Semiprozine',
-    description: `Any generally available non-professional periodical publication devoted to science fiction or fantasy, or related subjects which by the close of the previous calendar year has published four (4) or more issues (or the equivalent in other media), at least one (1) of which appeared in the previous calendar year, which does not qualify as a fancast, and which in the previous calendar year met at least one (1) of the following criteria: (1) paid its contributors and/or staff in other than copies of the publication, (2) was generally available only for paid purchase.`,
+    description: `Any generally available non-professional periodical publication devoted to science fiction or fantasy, or related subjects which by the close of ${YEAR} has published four (4) or more issues (or the equivalent in other media), at least one (1) of which appeared in the previous calendar year, which does not qualify as a fancast, and which in the previous calendar year met at least one (1) of the following criteria: (1) paid its contributors and/or staff in other than copies of the publication, (2) was generally available only for paid purchase.`,
     nominationFieldLabels: {
       title: 'Title'
     }
@@ -115,7 +117,7 @@ export const categoryInfo = {
 
   Fanzine: {
     title: 'Best Fanzine',
-    description: `Any generally available non-professional periodical publication devoted to science fiction, fantasy, or related subjects that by the close of the previous calendar year has published four (4) or more issues (or the equivalent in other media), at least one (1) of which appeared in the previous calendar year, that does not qualify as a semiprozine or a fancast, and that in the previous calendar year met neither of the following criteria: (1) paid its contributors or staff monetarily in other than copies of the publication, (2) was generally available only for paid purchase.`,
+    description: `Any generally available non-professional periodical publication devoted to science fiction, fantasy, or related subjects that by the close of ${YEAR} has published four (4) or more issues (or the equivalent in other media), at least one (1) of which appeared in ${YEAR}, that does not qualify as a semiprozine or a fancast, and that in ${YEAR} met neither of the following criteria: (1) paid its contributors or staff monetarily in other than copies of the publication, (2) was generally available only for paid purchase.`,
     nominationFieldLabels: {
       title: 'Title'
     }
@@ -123,7 +125,7 @@ export const categoryInfo = {
 
   Fancast: {
     title: 'Best Fancast',
-    description: `Any generally available non-professional audio or video periodical devoted to science fiction, fantasy, or related subjects that by the close of the previous calendar year has released four (4) or more episodes, at least one (1) of which appeared in the previous calendar year, and that does not qualify as a dramatic presentation.`,
+    description: `Any generally available non-professional audio or video periodical devoted to science fiction, fantasy, or related subjects that by the close of ${YEAR} has released four (4) or more episodes, at least one (1) of which appeared in ${YEAR}, and that does not qualify as a dramatic presentation.`,
     nominationFieldLabels: {
       title: 'Title'
     }
@@ -131,7 +133,7 @@ export const categoryInfo = {
 
   FanWriter: {
     title: 'Best Fan Writer',
-    description: `Any person whose writing has appeared in semiprozines or fanzines or in generally available electronic media during the previous calendar year.`,
+    description: `Any person whose writing has appeared in semiprozines or fanzines or in generally available electronic media during ${YEAR}.`,
     nominationFieldLabels: {
       author: 'Author',
       example: 'Example'
@@ -140,7 +142,7 @@ export const categoryInfo = {
 
   FanArtist: {
     title: 'Best Fan Artist',
-    description: `An artist or cartoonist whose work has appeared through publication in semiprozines or fanzines or through other public, non-professional, display (including at a convention or conventions), during the previous calendar year.`,
+    description: `An artist or cartoonist whose work has appeared through publication in semiprozines or fanzines or through other public, non-professional, display (including at a convention or conventions), during ${YEAR}.`,
     nominationFieldLabels: {
       author: 'Artist/Illustrator',
       example: 'Example'
@@ -149,7 +151,7 @@ export const categoryInfo = {
 
   Series: {
     title: 'Best Series',
-    description: `A multi-volume science fiction or fantasy story, unified by elements such as plot, characters, setting, and presentation, which has appeared in at least three (3) volumes consisting of a total of at least 240,000 words by the close of the calendar year 2016, at least one of which was published in 2016. If any series and a subset series thereof both receive sufficient nominations to appear on the final ballot, only the version which received more nominations shall appear.`,
+    description: `A multi-volume science fiction or fantasy story, unified by elements such as plot, characters, setting, and presentation, which has appeared in at least three (3) volumes consisting of a total of at least 240,000 words by the close of the calendar year ${YEAR}, at least one of which was published in ${YEAR}. If any series and a subset series thereof both receive sufficient nominations to appear on the final ballot, only the version which received more nominations shall appear.`,
     nominationFieldLabels: {
       title: 'Name of Series',
       author: 'Author',
@@ -160,7 +162,7 @@ export const categoryInfo = {
 
   NewWriter: {
     title: 'John W. Campbell Award',
-    description: `Award for the best new science fiction writer, sponsored by Dell Magazines (not a Hugo Award). A new writer is one whose first work of science fiction or fantasy appeared in 2015 or 2016 in a professional publication. For Campbell Award purposes, a professional publication is one for which more than a nominal amount was paid, any publication that had an average press run of at least 10,000 copies, or any other criteria that the Award sponsors may designate.`,
+    description: `Award for the best new science fiction writer, sponsored by Dell Magazines (not a Hugo Award). A new writer is one whose first work of science fiction or fantasy appeared in ${YEAR-1} or ${YEAR} in a professional publication. For Campbell Award purposes, a professional publication is one for which more than a nominal amount was paid, any publication that had an average press run of at least 10,000 copies, or any other criteria that the Award sponsors may designate.`,
     nominationFieldLabels: {
       author: 'Author',
       example: 'Example'
