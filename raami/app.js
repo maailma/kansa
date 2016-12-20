23f9a9cacf3dd7dccdd5b4bf2798fed9e9f6c0ee
Kimmo <kimmo@netzionale.com>
2016-12-20 11:32:38 +0200

    extend limits

--- raami/app.js
+++ raami/app.js
@@ -16,6 +16,8 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(bodyParser.json({limit: '2mb'}));
+app.use(bodyParser.urlencoded({limit: '2mb',extended: true, parameterLimit:2000}));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
