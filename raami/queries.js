4574d75e3ed9ee4e780ddbdb119f854c4d3fea2c
Kimmo <kimmo@netzionale.com>
2016-12-23 12:16:41 +0200

    save base64 in text

--- raami/queries.js
+++ raami/queries.js
@@ -24,8 +24,8 @@ module.exports = {
 };
 
 function getPeople(req, res, next) {
-  var _id = parseInt(req.params.id);
-  db.any("select person_id, name, continent, url, filename, encode(portfolio::bytea,'base64') as portfolio, description, transport, legal, auction, print, digital from Artist where person_id = $1", _id)
+  var _id = parseInt(req.params.id);                             
+  db.any("select id, person_id, name, continent, url, filename, filedata, description, transport, legal, auction, print, digital from Artist where person_id = $1", _id)
     .then(function (data) {
       res.status(200)
         .json(data);
@@ -48,7 +48,7 @@ function getArtists(req, res, next) {
 
 function getArtist(req, res, next) {
   var _id = parseInt(req.params.id);
-  db.one("select person_id, name, continent, url, filename, encode(portfolio::bytea,`base64`) as portfolio, description, transport, legal, auction, print, digital from Artist where id = $1", _id)
+  db.one("select person_id, name, continent, url, filename, filedata, description, transport, legal, auction, print, digital from Artist where id = $1", _id)
     .then(function (data) {
       res.status(200)
         .json(data);
@@ -61,8 +61,8 @@ function getArtist(req, res, next) {
 
 function createArtist(req, res, next) {
 
-  db.none("insert into Artist(person_id, name, continent, url, filename, portfolio, description, transport, legal, auction, print, digital)" +
-      "values(${person_id}, ${name}, ${continent}, ${url}, ${filename}, ${portfolio}, ${description}, ${transport}, ${legal}, ${auction}, ${print}, ${digital} )",
+  db.none("insert into Artist(person_id, name, continent, url, filename, filedata, description, transport, legal, auction, print, digital)" +
+      "values(${person_id}, ${name}, ${continent}, ${url}, ${filename}, ${filedata}, ${description}, ${transport}, ${legal}, ${auction}, ${print}, ${digital} )",
     req.body)
     .then(function () {
       res.status(200)
@@ -79,11 +79,11 @@ function createArtist(req, res, next) {
 
 function updateArtist(req, res, next) {
   var _id = parseInt(req.params.id);
-  db.none("update Artist set continent=$1, url=$2, filename=$3, portfolio=$4, name=$6, description=$6, transport=$7, legal=$8, auction=$9, print=$10, digital=$11 where id=$12",
+  db.none("update Artist set continent=$1, url=$2, filename=$3, filedata=$4, name=$6, description=$6, transport=$7, legal=$8, auction=$9, print=$10, digital=$11 where id=$12",
     [req.body.continent, 
      req.body.url,
      req.body.filename,
-     req.body.portfolio,
+     req.body.filedata,
      req.body.name,
      req.body.description,
      req.body.transport,
@@ -126,7 +126,7 @@ function updateArtist(req, res, next) {
 
 function getWorks(req, res, next) {
   var _id = parseInt(req.params.id);
-  db.any("select artist_id, title, width, height, gallery, orientation, technique, filename, encode(image::bytea, 'base64') as image, year, price from Works where artist_id = $1", _id)
+  db.any("select artist_id, title, width, height, gallery, orientation, technique, filename, filedata, year, price from Works where artist_id = $1", _id)
     .then(function (data) {
       res.status(200)
         .json({
@@ -140,7 +140,7 @@ function getWorks(req, res, next) {
 
 function getWork(req, res, next) {
   var _id = parseInt(req.params.id);
-  db.one("select artist_id, title, width, height, gallery, orientation, technique, filename, encode(image::bytea, 'base64') as image, year, price from Works where id = $1", _id)
+  db.one("select artist_id, title, width, height, gallery, orientation, technique, filename, filedata, year, price from Works where id = $1", _id)
     .then(function (data) {
       res.status(200)
         .json(data);
@@ -153,7 +153,7 @@ function getWork(req, res, next) {
 
 function createWork(req, res, next) {
   db.none("insert into Works(artist_id, title, width, height, gallery, orientation, technique, filename, image, year, price )" +
-      "values(${artist_id}, ${title}, ${width}, ${height}, ${gallery}, ${orientation}, ${technique}, ${filename}, ${image}, ${year}, ${price})",
+      "values(${artist_id}, ${title}, ${width}, ${height}, ${gallery}, ${orientation}, ${technique}, ${filename}, ${fildata}, ${year}, ${price})",
     req.body)
     .then(function () {
       res.status(200)
@@ -170,14 +170,14 @@ function createWork(req, res, next) {
 
 function updateWork(req, res, next) {
   var _id = parseInt(req.params.id)	
-  db.none("update Works set artist_id=$1, title=$2, width=$3, height=$4, gallery=$5, filename=$6, image=$7, price=$8, year=$9, orientation=$10, technique=$11 where id=$12",
+  db.none("update Works set artist_id=$1, title=$2, width=$3, height=$4, gallery=$5, filename=$6, filedata=$7, price=$8, year=$9, orientation=$10, technique=$11 where id=$12",
     [ req.body.artist_id,
       req.body.title, 
       req.body.width, 
       req.body.height, 
       req.body.gallery, 
       req.body.filename, 
-      req.body.image, 
+      req.body.filedata, 
       req.body.price,
       req.body.year,
       req.body.orientation,
