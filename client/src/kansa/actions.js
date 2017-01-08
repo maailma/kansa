cd51957c8d2dc99efb230ae858e7919fe653790b
Eemeli Aro <eemeli@gmail.com>
2017-01-08 17:24:24 +0200

    Add buyUpgrade action & API call

--- client/src/kansa/actions.js
+++ client/src/kansa/actions.js
@@ -1,3 +1,14 @@
+export const buyUpgrade = (id, membership, paper_pubs, amount, token, callback) => ({
+  module: 'kansa',
+  type: 'BUY_UPGRADE',
+  amount,
+  callback,
+  id,
+  membership,
+  paper_pubs,
+  token
+});
+
 export const getPrices = () => ({
   module: 'kansa',
   type: 'GET_PRICES'
