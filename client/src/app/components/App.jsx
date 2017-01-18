f1d1a553f7b7ee4f17ecfb5c5a69b61868572266
Eemeli Aro <eemeli@gmail.com>
2017-01-18 07:31:00 +0200

    App: Add link back to profiles

--- client/src/app/components/App.jsx
+++ client/src/app/components/App.jsx
@@ -3,20 +3,31 @@ import { connect } from 'react-redux'
 import { push } from 'react-router-redux'
 
 import FlatButton from 'material-ui/FlatButton'
+import IconButton from 'material-ui/IconButton'
 import Paper from 'material-ui/Paper'
 import Snackbar from 'material-ui/Snackbar'
+import ChevronLeftIcon from 'material-ui/svg-icons/navigation/chevron-left'
 import { Toolbar, ToolbarGroup, ToolbarTitle } from 'material-ui/Toolbar';
 
 import { hideMessage } from '../actions/app'
 import { logout } from '../actions/auth'
 
-const AppBar = ({ email, logout, title }) => <Paper zDepth={2} >
+const AppBar = ({ email, goToProfiles, logout, path, title }) => <Paper zDepth={2} >
   <Toolbar
     style={{
       backgroundColor: 'white'
     }}
   >
     <ToolbarGroup>
+      { path === '/profile' ? null : <IconButton
+          onTouchTap={goToProfiles}
+          style={{ marginLeft: -24 }}
+          tooltip='Member details'
+          tooltipPosition='bottom-right'
+          tooltipStyles={{ marginTop: -8 }}
+      >
+        <ChevronLeftIcon />
+      </IconButton> }
       <ToolbarTitle text={title} />
     </ToolbarGroup>
     <ToolbarGroup>
@@ -32,9 +43,11 @@ const AppBar = ({ email, logout, title }) => <Paper zDepth={2} >
   </Toolbar>
 </Paper>;
 
-const App = ({ children, email, hideMessage, logout, message, title }) => <div>
+const App = ({ children, email, goToProfiles, hideMessage, location, logout, message, title }) => <div>
   { email ? <AppBar
     email={email}
+    goToProfiles={goToProfiles}
+    path={location.pathname}
     logout={logout}
     title={title}
   /> : null }
@@ -54,6 +67,7 @@ export default connect(
     message: app.get('message'),
     title: app.get('title')
   }), {
+    goToProfiles: () => push('/profile'),
     hideMessage,
     logout
   }
