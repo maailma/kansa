a48971cab8bef9f1b7ea073f4eaa8b9349ce3f2d
Kimmo <kimmo@netzionale.com>
2017-01-29 11:28:36 +0200

    Access-Control-Allow-Credentials is true

--- raami/routes.js
+++ raami/routes.js
@@ -7,6 +7,7 @@ router.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", process.env.CORS_ORIGIN);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Credentials",'true')
   next();
  });
 
