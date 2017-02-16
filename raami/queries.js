98ed1b2b9b2f95229c139de22648b6ad3fecfb66
Kimmo <kimmo@netzionale.com>
2017-02-16 12:37:03 +0200

    WIP test POST on Kansa

--- raami/queries.js
+++ raami/queries.js
@@ -16,11 +16,12 @@ module.exports = {
 
 function access(req) {
   const id = parseInt(req.params.id);
-  // if (isNaN(id) || id < 0) return Promise.reject(new InputError('Bad id number'));
-  // if (!req.session || !req.session.user || !req.session.user.email) return Promise.reject(new AuthError());
+  console.log(id)
+  if (isNaN(id) || id < 0) return Promise.reject(new InputError('Bad id number'));
+  if (!req.session || !req.session.user || !req.session.user.email) return Promise.reject(new AuthError());
   return req.app.locals.db.oneOrNone('SELECT email FROM kansa.People WHERE id = $1', id)
     .then(data => {
-      // if (!data || !req.session.user.raami_admin && req.session.user.email !== data.email) throw new AuthError();
+      if (!data || !req.session.user.raami_admin && req.session.user.email !== data.email) throw new AuthError();
       return {
         id,
       };
@@ -54,39 +55,46 @@ function access(req) {
 // }
 
 function getArtist(req, res, next) {
-
-  var _id = req.params.id;
   access(req)
-  .then(req.app.locals.db.oneOrNone(`
-    SELECT *
-      FROM Artist
-     WHERE people_id = $1`, _id
-  ))
-  .then(function (data) {
+  .then(({id}) => {
+    // const id = req.params.id;
+    return req.app.locals.db.oneOrNone(`
+      SELECT *
+        FROM Artist
+       WHERE people_id = $1`, id
+    )
+    })
+  .then((data)=> {
+
       res.status(200)
         .json(data);
     })
-    .catch(next);
+  .catch(next);
 }
 
 
 function createArtist(req, res, next) {
   access(req)
-  .then(req.app.locals.db.one(`
-     INSERT INTO Artist
-                 (
-                   people_id, name, continent, url, filename, filedata,
-                   description, transport, legal, auction, print, digital, agent, contact, waitlist, postage
-                 )
-          VALUES (
-                   $(people_id), $(name), $(continent), $(url), $(filename),
-                   $(filedata), $(description), $(transport), $(legal),
-                   $(auction), $(print), $(digital), $(agent), $(contact), $(waitlist), $(postage)
-                 )
+  .then(({id}) => {
+    // const id = (req.params.id);
+    console.log(id)
+    return req.app.locals.db.one(`
+      INSERT INTO Artist
+                   (
+                     people_id, name, continent, url, filename, filedata,
+                     description, transport, legal, auction, print, digital, agent, contact, waitlist, postage
+                   )
+      VALUES (
+                     $(people_id), $(name), $(continent), $(url), $(filename),
+                     $(filedata), $(description), $(transport), $(legal),
+                     $(auction), $(print), $(digital), $(agent), $(contact), $(waitlist), $(postage)
+                   )
+      RETURNING people_id`, req.body
+      )
+    })
+    .then((data)=> {
+          console.log(data)
 
-       RETURNING people_id`, req.body
-  ))
-    .then(function (data) {
       res.status(200)
         .json({
           status: 'success',
@@ -94,7 +102,7 @@ function createArtist(req, res, next) {
         });
     })
     .catch(next);
-}
+  }
 
 
 // function updateArtist(req, res, next) {
@@ -155,13 +163,16 @@ function createArtist(req, res, next) {
 /**** WORKS ***/
 
 function getWorks(req, res, next) {
-  var _id = (req.params.id);
   access(req)
-  .then(req.app.locals.db.any(`
-    SELECT *
-      FROM Works
-     WHERE people_id = $1`, _id
-  ))
+  .then(() => {
+  const id = (req.params.id);
+
+    req.app.locals.db.any(`
+      SELECT *
+        FROM Works
+       WHERE people_id = $1`, id
+    )}
+  )
     .then(function (data) {
       res.status(200)
         .json({
