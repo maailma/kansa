e8d4783e81b1d37ec8f550ac34d8830c6cae4a34
Eemeli Aro <eemeli@gmail.com>
2017-03-06 23:42:36 +0200

    Refactor app:KeyRequest

--- client/src/app/components/KeyRequest.jsx
+++ client/src/app/components/KeyRequest.jsx
@@ -1,28 +1,23 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { routerShape, withRouter } from 'react-router'
-
+import { Card, CardHeader, CardText } from 'material-ui/Card'
 import RaisedButton from 'material-ui/RaisedButton'
 import TextField from 'material-ui/TextField'
 
 import { keyRequest } from '../actions/auth'
-import { PATH_IN } from '../../constants'
 
 class KeyRequest extends React.Component {
 
   static propTypes = {
-    email: React.PropTypes.string,
     keyRequest: React.PropTypes.func.isRequired,
-    router: routerShape.isRequired
   }
 
   state = {
     email: ''
   }
 
-  componentWillReceiveProps(nextProps) {
-    const { email, router } = nextProps;
-    if (email) router.replace(PATH_IN);
+  componentDidMount() {
+    this.focusRef && this.focusRef.focus();
   }
 
   render() {
@@ -30,33 +25,55 @@ class KeyRequest extends React.Component {
     const { email } = this.state;
     const validEmail = email && /.@.*\../.test(email);
 
-    return <form>
-      <TextField
-        id="email"
-        fullWidth={true}
-        floatingLabelText="Email"
-        value={email}
-        onChange={ev => this.setState({ email: ev.target.value })}
-      />
-      <div style={{ height: 32 }} />
-      <RaisedButton
-        label="Send login link"
-        fullWidth={true}
-        primary={true}
-        disabled={!validEmail}
-        style={{ margin: '12px 0' }}
-        onTouchTap={() => keyRequest(email)}
-      />
-    </form>
+    return <div>
+      <Card>
+        <CardHeader
+          title="Request login link"
+          style={{ fontWeight: 600 }}
+        />
+        <CardText>
+          <div>
+            To access our services, you'll need to use a login link sent to you
+            via email. To request a new login link, please enter your email
+            address below, and it'll be sent to you. The address you enter will
+            need to match the one we have in our database for you; it's the one
+            that you provided when signing up, and at which you've previously
+            received messages from us.
+          </div>
+          <TextField
+            id="email"
+            fullWidth={true}
+            floatingLabelText="Email"
+            ref={ ref => this.focusRef = ref }
+            value={email}
+            onChange={ev => this.setState({ email: ev.target.value })}
+          />
+          <RaisedButton
+            label="Send login link"
+            fullWidth={true}
+            primary={true}
+            disabled={!validEmail}
+            style={{ marginTop: 12 }}
+            onTouchTap={() => keyRequest(email)}
+          />
+        </CardText>
+      </Card>
+      <div style={{ fontSize: 14, marginBottom: 20, padding: '0 16px' }}>
+        <p>
+          If you're not able to enter a valid address, or if you do not receive
+          the login link within 30 minutes or so, please get in touch with us at
+          {' '}<a href="mailto:registration@worldcon.fi">registration@worldcon.fi</a>{' '}
+          and we'll help you get sorted.
+        </p><p>
+          If your email address is associated with more than one membership that
+          is eligible to nominate in the 2017 Hugo Awards, you'll need to use the
+          separately emailed Hugo Nominations login link to access your
+          nominations. For further assistance with Hugo nominations, please e-mail
+          {' '}<a href="mailto:hugohelp@worldcon.fi">hugohelp@worldcon.fi</a>.
+        </p>
+      </div>
+    </div>
   }
 }
 
-export default connect(
-  (state) => ({
-    email: state.user.get('email')
-  }), {
-    keyRequest
-  }
-)(
-  withRouter(KeyRequest)
-);
+export default connect(null, { keyRequest })(KeyRequest);
--- client/src/app/components/Login.jsx
+++ client/src/app/components/Login.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Paper from 'material-ui/Paper'
 const { Col, Row } = require('react-flexbox-grid');
 
 import KeyRequest from './KeyRequest'
@@ -15,31 +14,7 @@ export default () => <div>
       xs={8} xsOffset={2}
       sm={6} smOffset={3}
     >
-      <Paper
-        style={{
-          backgroundColor: '#fafafa',
-          padding: 24,
-        }}
-      >
-        To access our services, you'll need to use a login link sent to you via email. To request a new login link,
-        please enter your email address below, and it'll be sent to you. The address you enter will need to match the
-        one we have in our database for you; it's the one that you provided when signing up, and at which you've
-        previously received messages from us.
-        <KeyRequest/>
-      </Paper>
-      <p>
-        If you're not able to enter a valid address, or if you do not receive the login link within 30 minutes or so,
-        please get in touch with us at <a href="mailto:registration@worldcon.fi">registration@worldcon.fi</a> and we'll
-        help you get sorted.
-      </p>
-      <p>
-        If your email address is associated with more than one membership that is eligible to nominate in the 2017 Hugo
-        Awards, you'll need to use the separately emailed Hugo Nominations login link to access your nominations. For
-        further assistance with Hugo nominations, please e-mail <a href="mailto:hugohelp@worldcon.fi">hugohelp@worldcon.fi</a>.
-      </p>
-      <p>
-        To join Worldcon 75 as a new member, please use our <a href="https://shop.worldcon.fi/">online shop</a>.
-      </p>
+      <KeyRequest/>
     </Col>
   </Row>
 </div>;
