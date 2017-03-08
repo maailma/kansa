bd5f930f1c8249da1af645339fcfcf9c90cdd679
Eemeli Aro <eemeli@gmail.com>
2017-03-08 02:03:31 +0200

    Add JS_ROOT constant

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,1 +1,11 @@
 export const API_ROOT = API_HOST ? `https://${API_HOST}/api/` : '/api/';
+
+export const JS_ROOT = (() => {
+  let script = document.currentScript;
+  if (!script) {
+    const scripts = document.getElementsByTagName('script');
+    if (scripts.length === 0) return '/';
+    script = scripts[scripts.length - 1];
+  }
+  return script.src.replace(/\/[^\/]+$/, '');
+})();
