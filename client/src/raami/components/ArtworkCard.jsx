508c925ac61d89274e8fbdfebc6b73c06bfac14d
lebanonsky <github@babeknabel.fi>
2017-03-28 13:03:34 +0300

    Squash-merge branch 'raami' at d77b3a5 into dev

--- client/src/raami/components/ArtworkCard.jsx
+++ client/src/raami/components/ArtworkCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card'
+import Checkbox from 'material-ui/Checkbox';
 import FlatButton from 'material-ui/FlatButton';
 import MenuItem from 'material-ui/MenuItem';
 import SelectField from 'material-ui/SelectField';
@@ -24,7 +25,13 @@ export default class ArtworkCard extends React.Component {
       filedata: React.PropTypes.object,
       year: React.PropTypes.number,
       price: React.PropTypes.number,
-      gallery: React.PropTypes.string
+      gallery: React.PropTypes.string,
+      start: React.PropTypes.number,
+      sale: React.PropTypes.number,
+      right: React.PropTypes.boolean,
+      copies: React.PropTypes.number,
+      form: React.PropTypes.string,
+      original: React.PropTypes.boolean
     })
   };
 
@@ -72,6 +79,18 @@ export default class ArtworkCard extends React.Component {
       position: 'absolute'
     };
 
+    const labelStyle = {
+      color: '#888',
+      fontSize: 16
+    };
+
+    let prev_height = '0px';
+    
+    if( this.state.filedata) {
+
+      prev_height = '250px';
+    }
+
     return <Card style={this.props.style}>
       <CardText>
         <TextField
@@ -81,19 +100,7 @@ export default class ArtworkCard extends React.Component {
           onChange={(ev, title) => this.setState({ title })}
           value={this.state.title || ''}
         />
-        <SelectField
-          floatingLabelStyle={label}
-          floatingLabelText="Select Gallery"
-          fullWidth={true}
-          onChange={(ev, idx, gallery) => this.setState({ gallery })}
-          value={this.state.gallery || ''}
-        >
-          <MenuItem value={'Auction'} primaryText="Auction gallery" />
-          <MenuItem value={'Printshop'} primaryText="Printshop" />
-          <MenuItem value={'Digital'} primaryText="Digital gallery" />
-        </SelectField>
-
-        <div style={{ minHeight:'250px',display:'block',marginBottom:'20px'}} >
+        <div style={{ minHeight: prev_height, display:'block',marginBottom:'20px'}} >
           <span style={grey}>Preview image (max 2 MB)</span>
           <br/>
           <span className="upload" style={zindex}>
@@ -107,10 +114,10 @@ export default class ArtworkCard extends React.Component {
           <br/>
           {this.state.filedata && <img src={this.state.filedata} width="250px" />}
           <br/>
-          <span style={{position:'relative', top: '20px'}}>
-              <i>By uploading I give permission for this image to be <br/>
-              reproduced to promote the art show on the Worldcon 75 website, <br/>
-              social media accounts, and at the convention.</i>
+          <span style={{position:'relative', top: '20px', fontSize: 11 }}>
+              <i>By uploading I give permission for this image to be 
+              reproduced to promote the art show on the Worldcon 75 website, social media accounts,
+              and at the convention.</i>
             </span>
         </div>
 
@@ -178,6 +185,99 @@ export default class ArtworkCard extends React.Component {
           <MenuItem value={'Table'} primaryText="Table-top display" />
           <MenuItem value={'Wall'} primaryText="Wall mounted" />
         </SelectField>
+          <SelectField
+          floatingLabelStyle={label}
+          floatingLabelText="Select Gallery"
+          fullWidth={true}
+          onChange={(ev, idx, gallery) => this.setState({ gallery })}
+          value={this.state.gallery || ''}
+        >
+          <MenuItem value={'Auction'} primaryText="Auction gallery" />
+          <MenuItem value={'Printshop'} primaryText="Printshop" />
+          <MenuItem value={'Digital'} primaryText="Digital gallery" />
+        </SelectField>
+
+        { this.state.gallery == 'Auction' && 
+        <Row>
+        <Col xs={8}>
+          <Checkbox
+            checked={this.state.original || true }
+            label="Original item"
+            labelStyle={labelStyle}
+            onCheck={(ev, value) => this.setState({ original: value })}
+            style={{ marginTop: 20 }}
+          />
+      </Col>
+          <Col xs={6}>
+            <TextField
+              type="number"
+              name="start"
+              min = {0}
+              floatingLabelStyle={label}
+              floatingLabelText="Starting auction €"
+              fullWidth={true}
+              onChange={ev => this.setState({ start: Math.max(0, ev.target.value) })}
+              value={this.state.start || 0}
+            />           
+          </Col>
+          <Col xs={6}>
+            <TextField
+              type="number"
+              name="sale"
+              min = {0}
+              floatingLabelStyle={label}
+              floatingLabelText="Instant sale €"
+              fullWidth={true}
+              onChange={ev => this.setState({ sale: Math.max(0, ev.target.value) })}
+              value={this.state.sale || 0}
+            /> 
+          </Col>
+
+        </Row>
+        }
+        { this.state.gallery == 'Printshop' &&
+        <Row>
+          <Col xs={4}>
+            <TextField
+              type="number"
+              name="copies"
+              min = {0}
+              floatingLabelStyle={label}
+              floatingLabelText="No. of copies"
+              fullWidth={true}
+              onChange={ev => this.setState({ copies: Math.max(0, ev.target.value) })}
+              value={this.state.copies || 1}
+            />
+          </Col>
+          <Col xs={4}>
+            <TextField
+              type="number"
+              name="Price"
+              min = {0}
+              floatingLabelStyle={label}
+              floatingLabelText="Price €"
+              fullWidth={true}
+              onChange={ev => this.setState({ price: Math.max(0, ev.target.value) })}
+              value={this.state.price || 0}
+            />
+          </Col>
+          <Col xs={8}>
+            <SelectField
+            floatingLabelStyle={label}
+            floatingLabelText="Item format"
+            fullWidth={true}
+            onChange={(ev, idx, form) => this.setState({ form })}
+            value={this.state.form || ''}
+          >
+            <MenuItem value={'unframed'} primaryText="Unframed print" />
+            <MenuItem value={'framed'} primaryText="Framed print" />
+            <MenuItem value={'t-shirt'} primaryText="T-shirt" />
+            <MenuItem value={'mug'} primaryText="Mug" />
+            <MenuItem value={'other'} primaryText="Other (Please email us!)" />
+          </SelectField>
+          </Col>
+        </Row>
+      }
       </CardText>
 
       <CardActions>
--- client/src/raami/components/GalleryCard.jsx
+++ client/src/raami/components/GalleryCard.jsx
@@ -7,52 +7,83 @@ import RaisedButton from 'material-ui/RaisedButton'
 import TextField from 'material-ui/TextField'
 import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card'
 
+
 const labelStyle = {
   color: '#888',
   fontSize: 16
 };
 
-const GalleryCard = ({ artist: { auction, digital, legal, postage, print, waitlist }, onChange, onSave, style }) => {
-  let total = auction * 20 + print * 10;
+const GalleryCard = ({ artist: { auction, digital, legal, postage, print, waitlist, half }, onChange, onSave, style }) => {
+  let total = auction * 30 + half * 15 + print * 0.5;
   if (digital) total += 20;
   if (postage > 0) total += parseInt(postage) + 20;
-
   return <Card style={style}>
     <CardHeader style={{ fontWeight: 600 }} title="Reserve Gallery Space"/>
     <CardText style={{ fontSize: 16 }}>
       <Row style={{ alignItems: 'center' }}>
-        <Col xs={8}>
-          <label style={labelStyle}>Auction gallery</label>
+        <Col xs={8}>          
+          <label style={labelStyle}>Auction gallery wall panels</label>
         </Col>
         <Col xs={3}>
           <TextField
             fullWidth={true}
             inputStyle={{ textAlign: 'right' }}
             min={0}
+            max={4}
             name="auction"
             onChange={ev => onChange({ auction: Math.max(0, ev.target.value) })}
             type="number"
             value={auction}
           />
         </Col>
-        <Col xs={1}>m</Col>
-      </Row>
-      <Row style={{ alignItems: 'center' }}>
+        <Col xs={12}>
+        <span style={{ fontSize:13 }} >       
+        1 x 2.5 metres, of which 1 x 1.5 metres is optimally usable. Maximum of 4 per artist at 30 € each.
+        </span>
+        </Col>
         <Col xs={8}>
-          <label style={labelStyle}>Printshop gallery</label>
+          <label style={labelStyle}>Auction gallery table halfs</label>
         </Col>
         <Col xs={3}>
           <TextField
             fullWidth={true}
             inputStyle={{ textAlign: 'right' }}
             min={0}
+            max={8}
+            name="half"
+            onChange={ev => onChange({ half: Math.max(0, ev.target.value) })}
+            type="number"
+            value={half}
+          />
+        </Col>
+          <Col xs={12}>
+          <span style={{ fontSize:13 }} >       
+          100cm x 70cm. Maximum of 8 half-tables per artist at 15 € each.
+          </span>
+        </Col>
+      </Row>
+      <Row style={{ alignItems: 'center' }}>
+        <Col xs={8}>
+          <label style={labelStyle}>Printshop gallery copy items</label>
+        </Col>
+        <Col xs={3}>
+          <TextField
+            fullWidth={true}
+            inputStyle={{ textAlign: 'right' }}
+            min={0}
+            max={200}
             name="print"
             onChange={ev => onChange({ print: Math.max(0, ev.target.value) })}
             type="number"
             value={print}
           />
         </Col>
-        <Col xs={1}>m</Col>
+        <Col xs={1}>total</Col>
+        <Col xs={12}>
+                <span style={{ fontSize:13 }}>          
+           0.50 € per item copy. Maximum of 200 individual items per artist.
+           </span>
+        </Col>
       </Row>
       <Row>
         <Col xs={11} style={{ margin: '8px 0' }}>
@@ -99,7 +130,7 @@ const GalleryCard = ({ artist: { auction, digital, legal, postage, print, waitli
       <RaisedButton
         disabled={!legal}
         label="Save"
-        onClick={() => onSave(this.state)}
+        onClick={() => onSave()}
         primary={true}
         type="submit"
       />
@@ -115,6 +146,7 @@ GalleryCard.propTypes = {
     postage: React.PropTypes.number,
     print: React.PropTypes.number,
     waitlist: React.PropTypes.boolean,
+    half: React.PropTypes.number,
   }).isRequired,
   onChange: React.PropTypes.func.isRequired,
   onSave: React.PropTypes.func.isRequired,
