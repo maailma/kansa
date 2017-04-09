dd0ddd701da3d726a65734e88bb528fa91e6942d
Eemeli Aro <eemeli@gmail.com>
2017-04-09 16:18:25 +0300

    Various config fixes

--- postgres/init/90-dev-people-init.sql
+++ postgres/init/90-dev-people-init.sql
@@ -2,15 +2,15 @@ INSERT INTO admin.Admins VALUES ('admin@example.com', true, true, true);
 
 SET ROLE kansa;
 
-INSERT INTO People (legal_name, email, membership, member_number, can_hugo_nominate)
-     VALUES ('Admin', 'admin@example.com', 'NonMember', NULL, false),
-            ('First Member', 'member@example.com', 'FirstWorldcon', 21, true),
-            ('Fan Parent', 'family@example.com', 'Adult', 37, true),
-            ('Fan Child', 'family@example.com', 'Child', 45, false),
-            ('Fan Youth', 'family@example.com', 'Youth', 59, true),
-            ('Fan Supporter', 'supporter@example.com', 'Supporter', 68, true),
-            ('Dupe Supporter', 'supporter@example.com', 'Supporter', 76, false),
-            ('Fan Nominator', 'nominator@example.com', 'NonMember', NULL, true);
+INSERT INTO People (legal_name, email, membership, member_number, can_hugo_nominate, can_hugo_vote)
+     VALUES ('Admin', 'admin@example.com', 'NonMember', NULL, false, false),
+            ('First Member', 'member@example.com', 'FirstWorldcon', 21, true, true),
+            ('Fan Parent', 'family@example.com', 'Adult', 37, true, true),
+            ('Fan Child', 'family@example.com', 'Child', 45, false, false),
+            ('Fan Youth', 'family@example.com', 'Youth', 59, true, true),
+            ('Fan Supporter', 'supporter@example.com', 'Supporter', 68, true, true),
+            ('Dupe Supporter', 'supporter@example.com', 'Supporter', 76, false, false),
+            ('Fan Nominator', 'nominator@example.com', 'NonMember', NULL, true, false);
 
 INSERT INTO Keys
      VALUES ('admin@example.com', 'key'),
