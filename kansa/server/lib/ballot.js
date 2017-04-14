d30b81cd752e1297a3684df247594129dccaa4d0
Eemeli Aro <eemeli@gmail.com>
2017-04-14 19:03:54 +0300

    Update tuohi to eeemeli/pdf-form-fill:0.2

--- kansa/server/lib/ballot.js
+++ kansa/server/lib/ballot.js
@@ -3,18 +3,25 @@ const fetch = require('node-fetch');
 const ballotData = ({ member_number, legal_name, email, city, state, country, badge_text, paper_pubs, token }) => {
   const address = paper_pubs && paper_pubs.address.split(/[\n\r]+/) || [''];
   return {
-    Name: legal_name,
-    Address: address[0],
-    'Address (2nd line)': address.length > 1 ? address.slice(1).join('; ') : '',
-    City: city || '',
-    Country: paper_pubs ? paper_pubs.country : country || '',
-    'Membership number': member_number,
-    'Voting token': token,
-    'E-mail': email,
-    'State/Province/Prefecture': state || '',
-    'Badge name': badge_text || '',
-    'Voting fee': 'Yes',
-    Member: 'Yes'
+    info: {
+      Title: 'Ballot for ' + legal_name,
+      Author: 'Worldcon 77 Site Selection',
+      Creator: 'members.worldcon.fi'
+    },
+    fields: {
+      'Name': legal_name,
+      'Address': address[0],
+      'Address (2nd line)': address.length > 1 ? address.slice(1).join('; ') : '',
+      'City': city || '',
+      'Country': paper_pubs ? paper_pubs.country : country || '',
+      'Membership number': member_number,
+      'Voting token': token,
+      'E-mail': email,
+      'State/Province/Prefecture': state || '',
+      'Badge name': badge_text || '',
+      'Voting fee': 'Yes',
+      'Member': 'Yes'
+    }
   }
 }
 
