7d007dcdabe3944e5d99ac55de6eeaaccf1d613b
Eemeli Aro <eemeli@gmail.com>
2017-04-15 11:43:11 +0300

    Split router from index

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { IndexRedirect, IndexRoute, Redirect, Router, Route, browserHistory, hashHistory } from 'react-router'
+import { browserHistory, hashHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 import { createStore } from 'redux'
 
@@ -13,22 +13,9 @@ import './lib/polyfills'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
-import { keyLogin, tryLogin } from './app/actions/auth'
-import App from './app/components/App'
-import Index from './app/components/Index'
-import NewMemberForm from './membership/components/NewMemberForm'
-import NewMemberIndex from './membership/components/NewMemberIndex'
-import Upgrade from './membership/components/Upgrade'
-import Payments from './payments'
-import NewPayment from './payments/new-payment'
-import Canon from './hugo-admin/components/Canon'
-import Nominate from './hugo-nominations/components/Nominate'
-import Vote from './hugo-votes'
-import Finalists from './hugo-admin/components/Finalists'
-import HugoAdmin from './hugo-admin/components/HugoAdmin'
-import ExhibitRegistration from './raami/components/Registration'
 import middleware from './middleware'
 import reducers from './reducers'
+import AppRouter from './router'
 import { theme } from './theme'
 
 const history = ENV === 'production' ? browserHistory : hashHistory;
@@ -46,69 +33,10 @@ history.listen(({ pathname }) => {
   ga('send', 'pageview');
 });
 
-function checkAuth(nextState, replace, callback) {
-  const email = store.getState().user.get('email');
-  if (email) callback();
-  else store.dispatch(tryLogin(() => callback()));
-}
-
-function requireAuth({ location: { pathname }}, replace) {
-  const email = store.getState().user.get('email');
-  if (!email && pathname !== '/') replace('/');
-}
-
-function doLogin({ location: { query }, params: { email, key, id } }) {
-  const next = query && query.next || (id ? `/hugo/vote/${id}` : null);
-  store.dispatch(keyLogin(email, key, next));
-}
-
-const scrollUpOnChange = (_, { location: { action } }) => {
-  if (action !== 'POP') window.scrollTo(0, 0);
-}
-
 ReactDOM.render(
   <Provider store={store} >
     <MuiThemeProvider muiTheme={theme}>
-      <Router history={syncHistoryWithStore(history, store)}>
-        <Route path="/login/:email/:key" onEnter={doLogin} />
-        <Route path="/login/:email/:key/:id" onEnter={doLogin} />
-        <Route path="/" component={App} onChange={scrollUpOnChange} onEnter={checkAuth} >
-          <IndexRoute component={Index} />
-          <Redirect from="login" to="/" />
-          <Redirect from="profile" to="/" />
-          <Route path="new" component={NewMemberIndex} />
-          <Route path="new/:membership" component={NewMemberForm} />
-          <Route path="hugo" >
-            <IndexRedirect to="vote" />
-            <Route path="admin" onEnter={requireAuth} component={HugoAdmin}>
-              <IndexRedirect to='Novel' />
-              <Route path=":category">
-                <IndexRedirect to='nominations' />
-                <Route path="finalists" component={Finalists} />
-                <Route path="nominations" component={Canon} />
-              </Route>
-            </Route>
-            <Route path="nominate/:id" onEnter={requireAuth} component={Nominate} />
-            <Route path="vote">
-              <IndexRoute component={Vote} />
-              <Route path=":id" component={Vote} />
-            </Route>
-            <Redirect from=":id/nominate" to="nominate/:id" />
-            <Redirect from=":id/vote" to="vote/:id" />
-          </Route>
-          <Route onEnter={requireAuth}>
-            <Route path="exhibition/:id" component={ExhibitRegistration} />
-            <Route path="pay">
-              <IndexRoute component={Payments} />
-              <Route path=":type" component={NewPayment} />
-            </Route>
-            <Route path="upgrade">
-              <IndexRoute component={Upgrade} />
-              <Route path=":id" component={Upgrade} />
-            </Route>
-          </Route>
-        </Route>
-      </Router>
+      <AppRouter history={syncHistoryWithStore(history, store)} />
     </MuiThemeProvider>
   </Provider>,
   document.getElementById('react')
---
+++ client/src/router.jsx
@@ -0,0 +1,103 @@
+import React, { PropTypes } from 'react'
+import { IndexRedirect, IndexRoute, Redirect, Route, Router } from 'react-router'
+
+import { keyLogin, tryLogin } from './app/actions/auth'
+
+import App from './app/components/App'
+import Index from './app/components/Index'
+import Canon from './hugo-admin/components/Canon'
+import Nominate from './hugo-nominations/components/Nominate'
+import Vote from './hugo-votes'
+import Finalists from './hugo-admin/components/Finalists'
+import HugoAdmin from './hugo-admin/components/HugoAdmin'
+import NewMemberForm from './membership/components/NewMemberForm'
+import NewMemberIndex from './membership/components/NewMemberIndex'
+import Upgrade from './membership/components/Upgrade'
+import Payments from './payments'
+import NewPayment from './payments/new-payment'
+import ExhibitRegistration from './raami/components/Registration'
+
+const hugoRoutes = (path, requireAuth) => (
+  <Route path={path} >
+    <IndexRedirect to="vote" />
+    <Route path="admin" onEnter={requireAuth} component={HugoAdmin}>
+      <IndexRedirect to='Novel' />
+      <Route path=":category">
+        <IndexRedirect to='nominations' />
+        <Route path="finalists" component={Finalists} />
+        <Route path="nominations" component={Canon} />
+      </Route>
+    </Route>
+    <Route path="nominate/:id" onEnter={requireAuth} component={Nominate} />
+    <Route path="vote">
+      <IndexRoute component={Vote} />
+      <Route path=":id" component={Vote} />
+    </Route>
+    <Redirect from=":id/nominate" to="nominate/:id" />
+    <Redirect from=":id/vote" to="vote/:id" />
+  </Route>
+)
+
+export default class AppRouter extends Route {
+  static contextTypes = {
+    store: PropTypes.shape({
+      dispatch: PropTypes.func.isRequired,
+      getState: PropTypes.func.isRequired
+    }).isRequired
+  }
+
+  get dispatch() {
+    return this.context.store.dispatch
+  }
+
+  get userEmail() {
+    return this.context.store.getState().user.get('email')
+  }
+
+  checkAuth = (nextState, replace, callback) => {
+    if (this.userEmail) callback()
+    else this.dispatch(tryLogin(() => callback()))
+  }
+
+  doLogin = ({ location: { query }, params: { email, key, id } }) => {
+    const next = query && query.next || (id ? `/hugo/vote/${id}` : null)
+    this.dispatch(keyLogin(email, key, next))
+  }
+
+  requireAuth = ({ location: { pathname }}, replace) => {
+    if (!this.userEmail && pathname !== '/') replace('/');
+  }
+
+  scrollUpOnChange = (_, { location: { action } }) => {
+    if (action !== 'POP') window.scrollTo(0, 0)
+  }
+
+  render() {
+    const { history } = this.props
+    return (
+      <Router history={history}>
+        <Route path="/login/:email/:key" onEnter={this.doLogin} />
+        <Route path="/login/:email/:key/:id" onEnter={this.doLogin} />
+        <Route path="/" component={App} onChange={this.scrollUpOnChange} onEnter={this.checkAuth} >
+          <IndexRoute component={Index} />
+          <Redirect from="login" to="/" />
+          <Redirect from="profile" to="/" />
+          <Route path="new" component={NewMemberIndex} />
+          <Route path="new/:membership" component={NewMemberForm} />
+          {hugoRoutes('hugo', this.requireAuth)}
+          <Route onEnter={this.requireAuth}>
+            <Route path="exhibition/:id" component={ExhibitRegistration} />
+            <Route path="pay">
+              <IndexRoute component={Payments} />
+              <Route path=":type" component={NewPayment} />
+            </Route>
+            <Route path="upgrade">
+              <IndexRoute component={Upgrade} />
+              <Route path=":id" component={Upgrade} />
+            </Route>
+          </Route>
+        </Route>
+      </Router>
+    )
+  }
+}
