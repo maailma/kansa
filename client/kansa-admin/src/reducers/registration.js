95ff9cb04abd7f886231787e05852c7086aec08c
Eemeli Aro <eemeli@gmail.com>
2017-07-23 17:38:05 +0300

    kansa-admin: Add registration reducer

---
+++ client/kansa-admin/src/reducers/registration.js
@@ -0,0 +1,22 @@
+import { Map } from 'immutable'
+
+const defaultState = Map({
+  locked: false,
+  password: '',
+  printer: ''
+})
+
+export default function(state = defaultState, action) {
+  if (action.error) return state
+  switch (action.type) {
+
+    case 'SET REG LOCK':
+      return state.set('locked', !!action.locked)
+
+    case 'SET REG OPTIONS':
+      const { password, printer } = action
+      return state.merge({ password, printer })
+
+  }
+  return state
+}
