2858af9d804a94f86f806be1c41e63bb09d09258
Matthew. B. Gray <hey@matthew.nz>
2018-08-11 20:04:37 +1200

    Whitespace refactor, multiline membership status

--- config/database/21-member-types.sql
+++ config/database/21-member-types.sql
@@ -1,6 +1,13 @@
 SET ROLE kansa;
 
 CREATE TYPE MembershipStatus AS ENUM (
-    'NonMember', 'Exhibitor', 'Helper', 'Supporter', 'KidInTow',
-    'Child', 'Youth', 'FirstWorldcon', 'Adult'
+    'NonMember',
+    'Exhibitor',
+    'Helper',
+    'Supporter',
+    'KidInTow',
+    'Child',
+    'Youth',
+    'FirstWorldcon',
+    'Adult'
 );
