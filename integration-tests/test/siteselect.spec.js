6e361066c0c99936b53aad6b8f69474060327856
Eemeli Aro <eemeli@gmail.com>
2018-09-08 22:30:47 +0300

    Add tests for site-selection token & voter lists

--- integration-tests/test/siteselect.spec.js
+++ integration-tests/test/siteselect.spec.js
@@ -38,14 +38,42 @@ describe('Site selection', () => {
       })
   })
 
-  it('member: get own ballot', () => {
-    return member
+  it('member: get own ballot', () =>
+    member
       .get(`/api/people/${id}/ballot`)
       .expect(200)
-      .expect('Content-Type', 'application/pdf')
-  })
+      .expect('Content-Type', 'application/pdf'))
 
-  it("member: fail to get others' ballot", () => {
-    return member.get(`/api/people/${id - 1}/ballot`).expect(401)
-  })
+  it("member: fail to get others' ballot", () =>
+    member.get(`/api/people/${id - 1}/ballot`).expect(401))
+
+  it('member: fail to list tokens', () =>
+    member.get(`/api/siteselect/tokens.json`).expect(401))
+
+  it('member: fail to list voters', () =>
+    member.get(`/api/siteselect/voters.json`).expect(401))
+
+  it('admin: list tokens as JSON', () =>
+    admin
+      .get(`/api/siteselect/tokens.json`)
+      .expect(200)
+      .expect(res => assert(Array.isArray(res.body))))
+
+  it('admin: list tokens as CSV', () =>
+    admin
+      .get(`/api/siteselect/tokens.csv`)
+      .expect(200)
+      .expect('Content-Type', /text\/csv/))
+
+  it('admin: list voters as JSON', () =>
+    admin
+      .get(`/api/siteselect/voters.json`)
+      .expect(200)
+      .expect(res => assert(Array.isArray(res.body))))
+
+  it('admin: list voters as CSV', () =>
+    admin
+      .get(`/api/siteselect/voters.csv`)
+      .expect(200)
+      .expect('Content-Type', /text\/csv/))
 })
