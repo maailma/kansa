93bab25c2de2bc058dc99a5b393d329cae605cb4
Eemeli Aro <eemeli@gmail.com>
2018-09-11 22:07:41 +0300

    Merge server root router into module loader

--- server/lib/modules.js
+++ server/lib/modules.js
@@ -2,18 +2,34 @@ const debug = require('debug')('kansa:server')
 const path = require('path')
 
 const config = require('@kansa/common/config')
-const appRouter = require('./router')
+const adminRouter = require('./admin/router')
+const getConfig = require('./get-config')
+const peopleRouter = require('./people/router')
+const userRouter = require('./user/router')
 
 module.exports = (db, app) => {
   const ctx = {}
-  app.use('/', appRouter(db, ctx))
+
+  app.get('/config', (req, res, next) =>
+    getConfig(db)
+      .then(data => res.json(data))
+      .catch(next)
+  )
+
+  app.use(userRouter(db, ctx))
+
+  const ar = adminRouter(db)
+  app.use('/members', ar.membersRouter)
+  app.use('/people', ar)
+  app.use('/people', peopleRouter(db, ctx))
+
   Object.keys(config.modules).forEach(name => {
     const mc = config.modules[name]
     if (!mc) return
     debug(`Adding module ${name}`)
     const mp = path.resolve(__dirname, '..', 'modules', name)
-    const module = require(mp)
-    app.use(`/${name}`, module(db, ctx, mc))
+    app.use(`/${name}`, require(mp)(db, ctx, mc))
   })
+
   return ctx
 }
